6643cf387418e81dd4c4c1cb37adfcc7e1b80c11
doctafaustus <clowd150@gmail.com>
2017-11-04 23:13:26 -0500

    Add server

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
 var express = require('express');
 var request = require('request');
 var bodyParser = require('body-parser');
+
+
 var app = express();
-var urlencodedParser = bodyParser.urlencoded({ extended: false });
+app.use(bodyParser.urlencoded({ extended: true, limit: '1mb' }));
+app.use(bodyParser.json({limit: '1mb'}));
+app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'ejs');
+
+
+app.get('/', function(req, res){
+  	console.log('test!');
+  	res.sendStatus(200);
+});
+
+
+app.listen(process.env.PORT || 3000, function() {
+	console.log('App listening on port 3000');
+});
