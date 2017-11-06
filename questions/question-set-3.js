a29f10baceaef7f8a464be61e1b33e8f78c02277
doctafaustus <clowd150@gmail.com>
2017-11-05 22:10:36 -0600

    Create question-set-3.js

---
+++ questions/question-set-3.js
@@ -0,0 +1,82 @@
+module.exports = [
+  {
+    question: 'How long is an eon in astronomy?', 
+    a: 'A thousand years', 
+    b: 'A trillion years',
+    c: 'A million years',
+    d: 'A billion years',
+    answer: 'd'
+  },
+  {
+    question: 'What was Nicole Polizzi\'s nickname on "Jersey Shore"?', 
+    a: 'Sweetheart', 
+    b: 'JWoww',
+    c: 'Snooki',
+    d: 'The Situation',
+    answer: 'c'
+  },
+  {
+    question: 'What was the name of the U.S. mail service that started in 1860, ', 
+    a: 'Pony Express', 
+    b: 'Rider mail',
+    c: 'Fast mail',
+    d: 'U.S. Mail',
+    answer: 'a'
+  },
+  {
+    question: 'Where would you find the Sea of Tranquility?', 
+    a: 'Japan', 
+    b: 'The Moon',
+    c: 'Pluto',
+    d: 'The Middle East',
+    answer: 'b'
+  },
+  {
+    question: 'Which chess piece can only move diagonally?', 
+    a: 'King', 
+    b: 'Knight',
+    c: 'Rook',
+    d: 'Bishop',
+    answer: 'd'
+  },
+  {
+    question: 'When did the Cold War end?', 
+    a: '1993', 
+    b: '1991',
+    c: '1992',
+    d: '1990',
+    answer: 'b'
+  },
+  {
+    question: 'Which country is Prague in? ', 
+    a: 'Austria', 
+    b: 'Hungary',
+    c: 'Czech Republic',
+    d: 'Germany',
+    answer: 'c'
+  },
+  {
+    question: 'Where is Kanye West from?', 
+    a: 'St. Louis, MO', 
+    b: 'Miami, FL',
+    c: 'New York, NY',
+    d: 'Chicago, IL',
+    answer: 'd'
+  },
+  {
+    question: 'What was the first music video played on MTV?', 
+    a: 'We Are the World', 
+    b: 'Take On Me',
+    c: 'Video Killed The Radio Star',
+    d: 'Money for Nothing',
+    answer: 'c'
+  },
+  {
+    question: 'In computer science, what does "API" stand for?', 
+    a: 'Applied Programming Information ',
+    b: 'Application Program Interface',
+    c: 'Active Program Interface',
+    d: 'Application Programming Index',
+    answer: 'b'
+  },
+];
