af25c3546f271230d7ee14551a64f517aba69bed
doctafaustus <clowd150@gmail.com>
2017-11-08 12:36:14 -0600

    Configure question set 4

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ var webhookURL = (isProduction) ? 'https://hooks.slack.com/services/T02A4DYBJ/B7
 
 
 
-var questions = require('./questions/question-set-3');
+var questions = require('./questions/question-set-4');
 
 
 class Game {
