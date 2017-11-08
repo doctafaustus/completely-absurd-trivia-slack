2b610dd425ac40b12ecd1771d28949335b6a1aa8
doctafaustus <clowd150@gmail.com>
2017-11-07 22:42:38 -0600

    Add question set 5

---
+++ questions/question-set-5.js
@@ -0,0 +1,82 @@
+module.exports = [
+  {
+    question: 'What does the French word "framboise" mean in English?', 
+    a: 'Glass',
+    b: 'Liquor',
+    c: 'Raspberry',
+    d: 'Wine',
+    answer: 'c'
+  },
+  {
+    question: 'Who was the first American to orbit the earth?', 
+    a: 'John Glenn',
+    b: 'Jim Lovell',
+    c: 'Neil Armstrong',
+    d: 'Buzz Aldrin',
+    answer: 'a'
+  },
+  {
+    question: 'What is Prince\'s real name?', 
+    a: 'Michael David Testolini',
+    b: 'Prince David Pierce',
+    c: 'Joe Coppolo',
+    d: 'Prince Rogers Nelson',
+    answer: 'd'
+  },
+  {
+    question: 'How many chambers does the human heart have?', 
+    a: 'Two',
+    b: 'Four',
+    c: 'Six',
+    d: 'Eight',
+    answer: 'b'
+  },
+  {
+    question: 'What is the capital of Brazil?', 
+    a: 'Rio de Janeiro',
+    b: 'Caracas',
+    c: 'Brasilia',
+    d: 'San Jose',
+    answer: 'c'
+  },
+  {
+    question: 'Who is the largest manufacturer of eyewear?', 
+    a: 'Luxottica',
+    b: 'Conglomo',
+    c: 'Globex',
+    d: 'Audemars Piguet',
+    answer: 'a'
+  },
+  {
+    question: 'What was the first mobile phone that included a touch screen?', 
+    a: 'Apple iPhone',
+    b: 'Blackberry Storm',
+    c: 'IBM Simon',
+    d: 'Nokia 8',
+    answer: 'c'
+  },
+  {
+    question: 'Almost every episode of Seinfeld contains an image or reference to what superhero?', 
+    a: 'Batman',
+    b: 'Superman',
+    c: 'Spiderman',
+    d: 'Nicholas Cage',
+    answer: 'b'
+  },
+  {
+    question: 'How many bits are in a byte?', 
+    a: 'Four',
+    b: 'Six',
+    c: 'Eight',
+    d: 'Ten',
+    answer: 'c'
+  },
+  {
+    question: 'Which of the following words means "to kiss and cuddle amorously"?', 
+    a: 'Mugwump',
+    b: 'Canoodle',
+    c: 'Snollygost',
+    d: 'Taradiddle',
+    answer: 'b'
+  },
+];
