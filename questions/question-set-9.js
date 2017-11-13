578d9cf64cb0d9ff49383a45f384f6d40a4442cc
doctafaustus <clowd150@gmail.com>
2017-11-12 21:13:03 -0600

    Add question set 9

---
+++ questions/question-set-9.js
@@ -0,0 +1,82 @@
+module.exports = [
+  {
+    question: 'What rapper often wears a large clock necklace during his performances?', 
+    a: 'Flavor Flav',
+    b: 'Chuck D',
+    c: 'Ice Cube',
+    d: '50 Cent',
+    answer: 'a'
+  },
+  {
+    question: 'Where does the Euphrates River flow through?', 
+    a: 'Iraq',
+    b: 'Romania',
+    c: 'Yemen',
+    d: 'Egypt',
+    answer: 'a'
+  },
+  {
+    question: '"Betcha can\'t eat just one." was the slogan for what snack?', 
+    a: 'Fritos',
+    b: 'Lay\'s Potato Chips',
+    c: 'Wheat Thins',
+    d: 'Ritz Crackers',
+    answer: 'b'
+  },
+  {
+    question: 'Which poet wrote the famous poem "For Whom The Bell Tolls"?',
+    a: 'Lars Ulrich',
+    b: 'William Shakespeare',
+    c: 'John Donne',
+    d: 'Emily Dickinson',
+    answer: 'c'
+  },
+  {
+    question: '"Enokitake" is a type of what?', 
+    a: 'Mushroom',
+    b: 'Fish',
+    c: 'Tree',
+    d: 'Pear',
+    answer: 'a'
+  },
+  {
+    question: 'Which airline merged with United in 2012?', 
+    a: 'US Airways',
+    b: 'AirTran',
+    c: 'Northwest',
+    d: 'Continental',
+    answer: 'd'
+  },
+  {
+    question: 'What two-handed weapon combined a spear and battle-axe?', 
+    a: 'Morning Star',
+    b: 'Halberd',
+    c: 'Pike',
+    d: 'Ulfberht',
+    answer: 'b'
+  },
+  {
+    question: 'Which of these ingredients is NOT used in baklava?', 
+    a: 'Honey',
+    b: 'Chocolate',
+    c: 'Phyllo Dough',
+    d: 'Nuts',
+    answer: 'b'
+  },
+  {
+    question: 'Which hat is usually red and shaped like a truncated cone?', 
+    a: 'Fez',
+    b: 'Beret',
+    c: 'Boater',
+    d: 'Montera',
+    answer: 'a'
+  },
+  {
+    question: '"Hoggle" is a fictional character from what movie?',
+    a: 'Lord of the Rings',
+    b: 'Harry Potter',
+    c: 'Labyrinth',
+    d: 'The Neverending Story',
+    answer: 'c'
+  },
+];
