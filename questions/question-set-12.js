1f965457adb32753c92f7c1b501f43d2eefb744b
doctafaustus <clowd150@gmail.com>
2017-11-14 22:35:59 -0600

    Add question sets 12 and 13

---
+++ questions/question-set-12.js
@@ -0,0 +1,82 @@
+module.exports = [
+  {
+    question: 'What is the capital city of Canada?', 
+    a: 'Montreal',
+    b: 'Ottawa',
+    c: 'Vancouver',
+    d: 'Quebec City',
+    answer: 'b'
+  },
+  {
+    question: '"Lap Band" surgery is used to treat which condition?', 
+    a: 'Obesity',
+    b: 'Infertility',
+    c: 'Back Pain',
+    d: 'Hyperthyroidism',
+    answer: 'a'
+  },
+  {
+    question: 'Adolphus Busch was best known for starting what kind of company?', 
+    a: 'Oil',
+    b: 'Bank',
+    c: 'Sausage',
+    d: 'Beer',
+    answer: 'd'
+  },
+  {
+    question: 'In the popcorn industry, what term is used to refer to kernels that don\'t pop?', 
+    a: 'Old Maids',
+    b: 'Busts',
+    c: 'Geese',
+    d: 'Bombs',
+    answer: 'a'
+  },
+  {
+    question: 'What is the first name of former U.S. president Garfield?', 
+    a: 'Benjamin',
+    b: 'John',
+    c: 'Andrew',
+    d: 'James',
+    answer: 'd'
+  },
+  {
+    question: 'Which of the following snakes is poisonous?', 
+    a: 'Garter',
+    b: 'Rat',
+    c: 'Boa',
+    d: 'Cobra',
+    answer: 'd'
+  },
+  {
+    question: 'Sherwood Forest is associated with what legend?', 
+    a: 'King Arthur',
+    b: 'Robin Hood',
+    c: 'The Hobbit',
+    d: 'Lord of the Rings',
+    answer: 'b'
+  },
+  {
+    question: 'Warren Buffett is the CEO of what company?', 
+    a: 'Fannie Mae',
+    b: 'Walmart',
+    c: 'Berkshire Hathaway',
+    d: 'General Electric',
+    answer: 'c'
+  },
+  {
+    question: 'What is the fear of dreams called?', 
+    a: 'Oneirophobia',
+    b: 'Agoraphobia',
+    c: 'Hemophobia',
+    d: 'Acrophobia',
+    answer: 'a'
+  },
+  {
+    question: 'Which sport was reintroduced into the Olympic games in 2016?', 
+    a: 'Golf',
+    b: 'Water Polo',
+    c: 'Badminton',
+    d: 'Archery',
+    answer: 'a'
+  },
+];
