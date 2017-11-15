925f6c2a9b6a9079aad3aad787b2e348783ba1aa
doctafaustus <clowd150@gmail.com>
2017-11-14 22:26:03 -0600

    Create question-set-13.js

---
+++ question-set-13.js
@@ -0,0 +1,82 @@
+module.exports = [
+  {
+    question: 'Who was the first Disney princess created?',
+    a: 'Belle',
+    b: 'Aurora',
+    c: 'Snow White',
+    d: 'Cinderella',
+    answer: 'c'
+  },
+  {
+    question: 'What kind of car was the "General Lee" from "The Dukes of Hazzard"?',
+    a: 'Dodge Charger',
+    b: 'Ford Mustang',
+    c: 'Chevrolet Camaro',
+    d: 'Dodge Challenger',
+    answer: 'a'
+  },
+  {
+    question: 'Which of the following items did Benjamin Franklin invent?',
+    a: 'Scissors',
+    b: 'Bifocals',
+    c: 'Zipper',
+    d: 'Printing Press',
+    answer: 'b'
+  },
+  {
+    question: 'How many Oscars has Tom Cruise won?',
+    a: 'Zero',
+    b: 'One',
+    c: 'Two',
+    d: 'Three',
+    answer: 'a'
+  },
+  {
+    question: 'What is the medical term for bad breath?',
+    a: 'Halitosis',
+    b: 'Dyspnea',
+    c: 'Epistaxis',
+    d: 'Edema',
+    answer: 'a'
+  },
+  {
+    question: 'A "weevil" is a type of what?',
+    a: 'Spellbook',
+    b: 'Wrench',
+    c: 'Beetle',
+    d: 'Sword',
+    answer: 'c'
+  },
+  {
+    question: 'How much data could fit on a standard 3.5-inch floppy disk?',
+    a: '192 MB',
+    b: '1.44 MB',
+    c: '192 KB',
+    d: '1.44 KB',
+    answer: 'b'
+  },
+  {
+    question: 'Which beer has the most calories?',
+    a: 'Budweiser Select',
+    b: 'Bud Light',
+    c: 'Beck\'s Premier Light',
+    d: 'Michelob Ultra',
+    answer: 'b'
+  },
+  {
+    question: 'What legendary creature creates no reflection in a mirror?',
+    a: 'Vampire',
+    b: 'Elf',
+    c: 'Gargoyle',
+    d: 'Leprechaun',
+    answer: 'a'
+  },
+  {
+    question: 'In George Orwell\'s novel "Animal Farm," what animals control the "government" of the farm?',
+    a: 'Cats',
+    b: 'Pigs',
+    c: 'Goats',
+    d: 'Sheep',
+    answer: 'b'
+  },
+];
