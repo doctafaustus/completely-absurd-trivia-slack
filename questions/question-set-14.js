e143ade7c6b60939e687a9c97cd8bf8e432ebc2c
doctafaustus <clowd150@gmail.com>
2017-11-15 21:47:36 -0600

    Add question sets 14 and 15 (and configure 14 for play)

---
+++ questions/question-set-14.js
@@ -0,0 +1,82 @@
+module.exports = [
+  {
+    question: 'In art, what material does batik use? ', 
+    a: 'Wax',
+    b: 'Clay',
+    c: 'Wood',
+    d: 'Beads',
+    answer: 'a'
+  },
+  {
+    question: 'Author George Sand famously dated:', 
+    a: 'Liszt',
+    b: 'Salieri',
+    c: 'Chopin',
+    d: 'Satie',
+    answer: 'c'
+  },
+  {
+    question: 'The Kenai Peninsula is part of what state?', 
+    a: 'Michigan',
+    b: 'Florida',
+    c: 'Massachusetts',
+    d: 'Alaska',
+    answer: 'd'
+  },
+  {
+    question: 'Aaliyah appeared in what movie shortly before her death?', 
+    a: 'Chicago',
+    b: 'Queen of the Damned',
+    c: 'Men in Black II',
+    d: '8 Mile',
+    answer: 'b'
+  },
+  {
+    question: 'Wood alcohol poisoning can be treated with:', 
+    a: 'Booze',
+    b: 'Saline',
+    c: 'Iodine',
+    d: 'Baking Soda',
+    answer: 'a'
+  },
+  {
+    question: 'Benadryl can be used both as an antihistamine and a(n) ______?', 
+    a: 'Antiemetic',
+    b: 'Anti-inflammatory',
+    c: 'Diuretic',
+    d: 'Analgesic',
+    answer: 'a'
+  },
+  {
+    question: 'Led Zeppelin wrote songs about the works of which author?', 
+    a: 'Nietzsche',
+    b: 'Poe',
+    c: 'Tolkein',
+    d: 'Wagner',
+    answer: 'c'
+  },
+  {
+    question: 'What does the Scandinavian liquor Akvavit taste like?', 
+    a: 'Cedar',
+    b: 'Anise',
+    c: 'Elderberry',
+    d: 'Pepper',
+    answer: 'b'
+  },
+  {
+    question: 'What is a combination of diced carrots, celery, and onion called in French cuisine?', 
+    a: 'Trois Couleurs',
+    b: 'Vraie Poutine',
+    c: 'Galette',
+    d: 'Mirepoix',
+    answer: 'd'
+  },
+  {
+    question: 'Which animal has its testicles above its dingaling?', 
+    a: 'Manatee',
+    b: 'Armadillo',
+    c: 'Kangaroo',
+    d: 'Sea Turtle',
+    answer: 'c'
+  },
+];
---
+++ questions/question-set-15.js
@@ -0,0 +1,82 @@
+module.exports = [
+  {
+    question: 'In the body luteinizing hormone is produced by what gland(s)?', 
+    a: 'Pituitary ',
+    b: 'Thyroid',
+    c: 'Adrenal ',
+    d: 'Mammary',
+    answer: 'a'
+  },
+  {
+    question: 'What color is closest to chartreuse?', 
+    a: 'Green',
+    b: 'Blue',
+    c: 'Purple',
+    d: 'Black',
+    answer: 'a'
+  },
+  {
+    question: 'According to the song, what did my true love give to me on the 11th day of Xmas', 
+    a: 'Maids a-Milking',
+    b: 'Drummers Drumming',
+    c: 'Pipers Piping',
+    d: 'Geese a-Laying',
+    answer: 'c'
+  },
+  {
+    question: 'Which of the following tools would a firefighter be most likely to use?', 
+    a: 'Jaws of Life',
+    b: 'Hurst',
+    c: 'Dremel',
+    d: 'Coping Saw',
+    answer: 'a'
+  },
+  {
+    question: 'Who originated The Uncertainty Principle?', 
+    a: 'Newton',
+    b: 'Heisenberg',
+    c: 'Bohr',
+    d: 'Hawking',
+    answer: 'b'
+  },
+  {
+    question: 'How long is a silver anniversary?', 
+    a: '10 Years',
+    b: '25 Years',
+    c: '50 Years',
+    d: '60 Years',
+    answer: 'b'
+  },
+  {
+    question: 'In the game of chess, which piece has the most freedom to move?', 
+    a: 'King',
+    b: 'Queen',
+    c: 'Knight',
+    d: 'Bishop',
+    answer: 'b'
+  },
+  {
+    question: 'What country are the members of the bad "Men Without Hats" originally from?', 
+    a: 'USA',
+    b: 'Canada',
+    c: 'Sweden',
+    d: 'Australia',
+    answer: 'b'
+  },
+  {
+    question: 'What is the heaviest land animal on Earth?', 
+    a: 'Rhinoceros',
+    b: 'Elephant',
+    c: 'Hippopotamus',
+    d: 'Bison',
+    answer: 'b'
+  },
+  {
+    question: 'What planet did Alf come from?', 
+    a: 'Melmac',
+    b: 'Remulak',
+    c: 'Ork',
+    d: 'Cybertron',
+    answer: 'a'
+  },
+];
