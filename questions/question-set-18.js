25abe0bbd890f86917f4a4d3314c6a294dd5486d
doctafaustus <clowd150@gmail.com>
2017-11-18 22:31:54 -0600

    Add Shannon's game

---
+++ questions/question-set-18.js
@@ -0,0 +1,82 @@
+module.exports = [
+  {
+    question: 'What is the closest galaxy to our own?', 
+    a: 'Milky Way',
+    b: 'Betelgeuse',
+    c: 'Andromeda',
+    d: 'NGC 1300',
+    answer: 'c'
+  },
+  {
+    question: 'Which of the following is known as a language isolate?', 
+    a: 'Urdu',
+    b: 'Kanji',
+    c: 'Catalan',
+    d: 'Basque',
+    answer: 'd'
+  },
+  {
+    question: 'What is the only ancient wonder that still exists?', 
+    a: 'Great Pyramid of Giza',
+    b: 'Colossus of Rhodes',
+    c: 'Great Wall of China',
+    d: 'Colosseum',
+    answer: 'a'
+  },
+  {
+    question: 'How tall was Napoleon Bonaparte?', 
+    a: '5\' 1"',
+    b: '5\' 5"',
+    c: '5\' 7"',
+    d: '5\' 8"',
+    answer: 'c'
+  },
+  {
+    question: 'Which of the following is a thicking sauce made up of flour and fat?',
+    a: 'Aioli',
+    b: 'Roux',
+    c: 'Hollandaise',
+    d: 'Crisco',
+    answer: 'b'
+  },
+  {
+    question: 'Who is the goddaughter of Dolly Parton?', 
+    a: 'Sara Evans',
+    b: 'Carrie Underwood',
+    c: 'Taylor Swift',
+    d: 'Miley Cyrus',
+    answer: 'd'
+  },
+  {
+    question: 'Which musician has a theme park named after him/her?', 
+    a: 'Bob Marley',
+    b: 'Dolly Parton',
+    c: 'Bob Dylan',
+    d: 'Art Garfunkel',
+    answer: 'b'
+  },
+  {
+    question: 'What serial killer was known "The Killer Clown"?', 
+    a: 'John Wayne Gacy',
+    b: 'Ted Bundy',
+    c: 'Jeffrey Dahmer',
+    d: 'Charles Manson',
+    answer: 'a'
+  },
+  {
+    question: 'What is Venus\'s Greek counterpart?', 
+    a: 'Aphrodite',
+    b: 'Brizo',
+    c: 'Calypso',
+    d: 'Demeter',
+    answer: 'a'
+  },
+  {
+    question: 'What 1996 children\'s movie starred Shaquille O\'Neal?', 
+    a: 'Kazaam',
+    b: 'Shazam!',
+    c: 'Space Jam',
+    d: 'Air Bud',
+    answer: 'a'
+  },
+];
