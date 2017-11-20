938fe856c5758b92f70817190b8357e5add46239
doctafaustus <clowd150@gmail.com>
2017-11-20 12:49:38 -0600

    Ready

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ var prodRealURL = 'https://hooks.slack.com/services/T02A4DYBJ/B7WHHJC05/2lPlwdnh
 
 var webhookURL = prodRealURL;
 
-var questions = require('./questions/question-set-who-said-it');
+var questions = require('./questions/question-set-18');
 
 
 // Trivia database API
