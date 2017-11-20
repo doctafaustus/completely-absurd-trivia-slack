3ea171c3105a5f59720b9e2378bf9ce7f72d9970
doctafaustus <clowd150@gmail.com>
2017-11-19 22:10:33 -0600

    Add 'Who said it?' question set

---
+++ questions/question-set-who-said-it.js
@@ -0,0 +1,82 @@
+module.exports = [
+  {
+    question: 'Who said: "Who likes nuts in their pie?"', 
+    a: 'Mercan',
+    b: 'Jane',
+    c: 'JoCo',
+    d: 'Adam',
+    answer: 'c'
+  },
+  {
+    question: 'Who said: "I\'m going to give you so many holes for this."', 
+    a: 'Billy',
+    b: 'JoCo',
+    c: 'Jane',
+    d: 'RA',
+    answer: 'd'
+  },
+  {
+    question: 'Who said: "But seriously, I don\'t trust the government."', 
+    a: 'Marion',
+    b: 'Jane',
+    c: 'Joshua',
+    d: 'RG',
+    answer: 'b'
+  },
+  {
+    question: 'Who said: "Why are all these chips mad exotic?"', 
+    a: 'Erica',
+    b: 'BDF',
+    c: 'Dante',
+    d: 'Chels-B',
+    answer: 'c'
+  },
+  {
+    question: 'Who said to West: "Do you have siblings? God, I bet you would be so annoying."', 
+    a: 'Marisa',
+    b: 'Shannon',
+    c: 'Kevin',
+    d: 'Troy',
+    answer: 'a'
+  },
+  {
+    question: 'Who said: "It\'s like airbnb, for your body."', 
+    a: 'Dante',
+    b: 'Mercan',
+    c: 'Troy',
+    d: 'Cahak',
+    answer: 'd'
+  },
+  {
+    question: 'Who said: "An alternate name for a hotdog is a tubesteak."', 
+    a: 'Jane',
+    b: 'JoCo',
+    c: 'Dante',
+    d: 'Mercan',
+    answer: 'c'
+  },
+  {
+    question: 'Who said: "I\'m more of a penguin. Slow and dumb, but cute."', 
+    a: 'Troy',
+    b: 'JoCo',
+    c: 'Shanwow',
+    d: 'Tomi',
+    answer: 'c'
+  },
+  {
+    question: 'Who said to an interview candidate: "Pricey Comfort? That would be my stripper name."', 
+    a: 'JoCo',
+    b: 'Jane',
+    c: 'Mike',
+    d: 'Dante',
+    answer: 'd'
+  },
+  {
+    question: 'Who said: "It\'s just like, we farted... but now there was a microphone next to it."', 
+    a: 'Shannon',
+    b: 'Mike',
+    c: 'Ashley',
+    d: 'RG',
+    answer: 'b'
+  },
+];
