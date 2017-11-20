ef31be39989e77cace0f348a3b8d396385ae557f
doctafaustus <clowd150@gmail.com>
2017-11-20 00:20:31 -0600

    Add question set 19

---
+++ questions/question-set-18.js
@@ -0,0 +1,82 @@
+module.exports = [
+  {
+    question: 'What board game involves a game piece called a "Bread Basket"?', 
+    a: 'Clue',
+    b: 'Stratego',
+    c: 'Mouse Trap',
+    d: 'Operation',
+    answer: 'd'
+  },
+  {
+    question: 'Which of the following is NOT a character from McDonald\'s "McDonaldland"?', 
+    a: 'Captain Crook',
+    b: 'Billy Burger',
+    c: 'Grimace',
+    d: 'Birdie',
+    answer: 'b'
+  },
+  {
+    question: 'Paul McCartney\'s daughter Stella, is famous in what industry?', 
+    a: 'Fashion',
+    b: 'Music',
+    c: 'Movies',
+    d: 'Literature',
+    answer: 'a'
+  },
+  {
+    question: 'What word describes a small settlement, generally one smaller than a village?', 
+    a: 'Estuary',
+    b: 'Hamlet',
+    c: 'Shire',
+    d: 'Canton',
+    answer: 'b'
+  },
+  {
+    question: 'What type of road is raised to allow access across low or swampy ground?', 
+    a: 'Parkway',
+    b: 'Turnpike',
+    c: 'Causeway',
+    d: 'Beltway',
+    answer: 'c'
+  },
+  {
+    question: 'On the show Beavis and Butt-Head, Beavis often transforms into what character?',
+    a: 'Stefan',
+    b: 'Duffman',
+    c: 'Mint-Berry Crunch',
+    d: 'Cornholio',
+    answer: 'd'
+  },
+  {
+    question: 'Who invented the World Wide Web?', 
+    a: 'Tim Berners-Lee',
+    b: 'Larry Page',
+    c: 'John Fanning',
+    d: 'Steve Wozniak',
+    answer: 'a'
+  },
+  {
+    question: 'How many bones exist within the human body?', 
+    a: '174',
+    b: '206',
+    c: '410',
+    d: '640',
+    answer: 'b'
+  },
+  {
+    question: 'Which alcoholic beverage is anise-flavored?', 
+    a: 'Sambuca',
+    b: 'Pimm\'s',
+    c: 'Hpnotiq',
+    d: 'Midori',
+    answer: 'a'
+  },
+  {
+    question: 'Where is the legendary town of Sleepy Hollow located?', 
+    a: 'New York',
+    b: 'Massachusetts',
+    c: 'Rhode Island',
+    d: 'New Hampshire',
+    answer: 'a'
+  },
+];
