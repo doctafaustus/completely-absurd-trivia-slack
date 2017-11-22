ec3846bcee31d53858e2070e03867e63f61b01ff
doctafaustus <clowd150@gmail.com>
2017-11-22 00:12:23 -0600

    Add question sets 21 and 22

---
+++ questions/question-set-21.js
@@ -0,0 +1,82 @@
+module.exports = [
+  {
+    question: 'Gin and tonics are a fun way to prevent what disease?', 
+    a: 'Malaria',
+    b: 'Giardia',
+    c: 'Sobriety',
+    d: 'Rickets',
+    answer: 'a'
+  },
+  {
+    question: 'The fictional "Three Laws of Robotics" were created by:', 
+    a: 'Le Guin',
+    b: 'Bradbury',
+    c: 'Asimov',
+    d: 'Bender',
+    answer: 'c'
+  },
+  {
+    question: 'How many tablespoons in a quarter cup?', 
+    a: '2.5',
+    b: '3',
+    c: '4',
+    d: '1.2 billion',
+    answer: 'c'
+  },
+  {
+    question: 'In a normal environment, a human can survive for ___ days without water. ', 
+    a: '1',
+    b: '3-5',
+    c: '11-15',
+    d: 'up to 21',
+    answer: 'b'
+  },
+  {
+    question: 'In hunting, the number of points a buck is worth is determined by its:', 
+    a: 'Weight',
+    b: 'Shoulder Height',
+    c: 'Shirt Size',
+    d: 'Horns',
+    answer: 'd'
+  },
+  {
+    question: 'Buddhism originated in what is now:', 
+    a: 'India',
+    b: 'China',
+    c: 'Japan',
+    d: 'Your Mom',
+    answer: 'a'
+  },
+  {
+    question: 'Sister Rosetta Tharpe basically invented:', 
+    a: 'Nunnery',
+    b: 'Ski-dancing',
+    c: 'Rock n\' Roll',
+    d: 'Rollerblades',
+    answer: 'c'
+  },
+  {
+    question: 'Rogaine was originally developed as a medication for:', 
+    a: 'Erectile Dysfunction',
+    b: 'Blood Pressure',
+    c: 'High Cholesterol',
+    d: 'Jock Itch',
+    answer: 'b'
+  },
+  {
+    question: 'The northern lights are caused by:', 
+    a: 'Sound Waves',
+    b: 'Chemtrails',
+    c: 'Hundreds of Bees',
+    d: 'Solar Flares',
+    answer: 'd'
+  },
+  {
+    question: 'In Calvin and Hobbes, what was Calvin\'s main super hero alter ego?', 
+    a: 'Underpants Man',
+    b: 'Zorcher',
+    c: 'King Dino',
+    d: 'Spaceman Spiff',
+    answer: 'd'
+  },
+];
---
+++ questions/question-set-22.js
@@ -0,0 +1,82 @@
+module.exports = [
+  {
+    question: 'What company manufactures the Core 2 Duo processor?', 
+    a: 'AMD',
+    b: 'Sun Microsystems',
+    c: 'Intel',
+    d: 'IBM',
+    answer: 'c'
+  },
+  {
+    question: '"Phobos" and "Deimos" are the names of ____:', 
+    a: 'Moons',
+    b: 'Roman Goddesses',
+    c: 'Microscopic Particles',
+    d: 'Ancient Egyptian Rulers',
+    answer: 'a'
+  },
+  {
+    question: 'Photosynthesis is carried out in which part of the cell?', 
+    a: 'Mitochondria',
+    b: 'Centrioles',
+    c: 'Chloroplasts',
+    d: 'Ribosomes',
+    answer: 'c'
+  },
+  {
+    question: 'A material that can be pressed into shape without breaking can also be described as:', 
+    a: 'Viscous',
+    b: 'Malleable',
+    c: 'Imperturbable',
+    d: 'Insatiable',
+    answer: 'b'
+  },
+  {
+    question: 'John Adams appears on which denomination of the U.S. dollar?', 
+    a: 'None',
+    b: '$2',
+    c: '$100',
+    d: '$5,000',
+    answer: 'a'
+  },
+  {
+    question: 'Which animal is endemic to Australia?', 
+    a: 'Emu',
+    b: 'Alpaca',
+    c: 'Llama',
+    d: 'Vicuna',
+    answer: 'a'
+  },
+  {
+    question: 'What year was YouTube launched?', 
+    a: '2004',
+    b: '2005',
+    c: '2006',
+    d: '2007',
+    answer: 'b'
+  },
+  {
+    question: 'The "Joint Photographic Experts Group" is best known for creating ____?', 
+    a: 'Photoshop',
+    b: 'GIMP',
+    c: 'A Digital Image Format',
+    d: 'SVG',
+    answer: 'c'
+  },
+  {
+    question: 'In which state would you find the Great Smoky Mountains?', 
+    a: 'Tennessee',
+    b: 'California',
+    c: 'Utah',
+    d: 'Idaho',
+    answer: 'a'
+  },
+  {
+    question: 'Who is NOT a character from the show "Muppet Babies"?', 
+    a: 'Scooter',
+    b: 'Frazzle',
+    c: 'Skeeter',
+    d: 'Bunsen',
+    answer: 'b'
+  },
+];
