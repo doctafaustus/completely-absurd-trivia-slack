8681542283e5d318376e5e319c8f5847cd0219a6
doctafaustus <clowd150@gmail.com>
2017-11-22 14:53:14 -0600

    Configure question set thanksgiving

---
+++ questions/question-set-thanksgiving.js
@@ -0,0 +1,82 @@
+module.exports = [
+  {
+    question: 'how long was the first thanksgiving celebration?', 
+    a: '2 hours',
+    b: 'a week',
+    c: 'a month',
+    d: '3 days',
+    answer: 'd'
+  },
+  {
+    question: 'the woman behind which song is also responsible for Thanksgiving’s recognition?', 
+    a: 'Twinkle Twinkle Little Star',
+    b: 'Hokey Pokey',
+    c: 'Mary Had a Little Lamb',
+    d: 'Old Macdonald',
+    answer: 'c'
+  },
+  {
+    question: 'What was missing from the first \'s Thanksgiving Day Parade? ', 
+    a: 'Turkey',
+    b: 'The crowd',
+    c: 'Alcohol',
+    d: 'Balloons',
+    answer: 'd'
+  },
+  {
+    question: 'A Thanksgiving mix-up inspired what?', 
+    a: 'Mashed Potatoes',
+    b: 'Pumpkin Pie',
+    c: 'TV Dinners',
+    d: 'Rumchatas',
+    answer: 'c'
+  },
+  {
+    question: 'Approximately how many turkeys are cooked for Thanksgiving each year?', 
+    a: '20 million',
+    b: '46 Million',
+    c: '3 Billion',
+    d: '800,000',
+    answer: 'b'
+  },
+  {
+    question: 'How many places in the US are named ‘turkey’?', 
+    a: 'none',
+    b: '2',
+    c: '4',
+    d: '10',
+    answer: 'c'
+  },
+  {
+    question: 'The Greek Goddess of Corn is?', 
+    a: 'Starchy',
+    b: 'Demeter',
+    c: 'Cleopatra',
+    d: 'Cornholia',
+    answer: 'b'
+  },
+  {
+    question: 'The term "Cornucopia" means what?', 
+    a: 'corn land',
+    b: 'city of corn',
+    c: 'starchy’s house',
+    d: 'horn of plenty',
+    answer: 'd'
+  },
+  {
+    question: 'In what year did the first Macy\'s Thanksgiving parade take place', 
+    a: '1919',
+    b: '1905',
+    c: '1928',
+    d: '1924',
+    answer: 'd'
+  },
+  {
+    question: 'Which booze did the pilgrims take with them on their voyage?', 
+    a: 'rum',
+    b: 'beer',
+    c: 'vodka',
+    d: 'whisky',
+    answer: 'b'
+  },
+];
