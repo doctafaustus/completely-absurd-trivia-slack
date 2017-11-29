6af16f1d537faff1df13a1cf96255e95671247f4
doctafaustus <clowd150@gmail.com>
2017-11-29 14:25:48 -0600

    Add and configure chelsB questions

---
+++ questions/question-set-23.js
@@ -0,0 +1,82 @@
+module.exports = [
+  {
+    question: 'Which female soccer player famously took off her shirt in the 1999 Women\'s World Cup?', 
+    a: 'Mia Hamm',
+    b: 'Kristine Lilly',
+    c: 'Hope Solo',
+    d: 'Brandi Chastain',
+    answer: 'd'
+  },
+  {
+    question: 'Which famous bachelor was recently engaged?', 
+    a: 'John Mayer',
+    b: 'Prince Harry',
+    c: 'George Clooney',
+    d: 'Zac Efron',
+    answer: 'b'
+  },
+  {
+    question: 'Which Goonies character recently appeared in the hit Netflix show Stranger Things?', 
+    a: 'Mouth',
+    b: 'Mikey',
+    c: 'Chunk',
+    d: 'Data',
+    answer: 'b'
+  },
+  {
+    question: 'Where is Dippin\' Dots (aka the Ice Cream of the future) headquartered?', 
+    a: 'South Burlington, VT',
+    b: 'Carlsbad, CA',
+    c: 'Paducah, KY',
+    d: 'Oak Brook, IL',
+    answer: 'c'
+  },
+  {
+    question: 'In the show The Office, the K in Dwight K. Schrute stands for what?', 
+    a: 'Kyle',
+    b: 'Karl',
+    c: 'Kurt',
+    d: 'Klaus',
+    answer: 'c'
+  },
+  {
+    question: 'At what age did Michael Phelps first qualify for the Olympics?', 
+    a: '13',
+    b: '15',
+    c: '16',
+    d: '18',
+    answer: 'b'
+  },
+  {
+    question: 'Sean Carey and Justin Vernon of Bon Iver are both from what small town?', 
+    a: 'Eau Claire, WI',
+    b: 'White Bear Lake, MN',
+    c: 'Star City, IN',
+    d: 'Montoursville, PA',
+    answer: 'a'
+  },
+  {
+    question: 'Who was the most googled political candidate of 2016?', 
+    a: 'Hillary Clinton',
+    b: 'Ted Cruz',
+    c: 'Bernie Sanders',
+    d: 'Donald Trump',
+    answer: 'd'
+  },
+  {
+    question: 'Of the following words from Harry Potter, which is not in the dictionary?', 
+    a: 'Pensieve',
+    b: 'Dumbledore',
+    c: 'Muggle',
+    d: 'Arithmancy',
+    answer: 'a'
+  },
+  {
+  question: 'On the famous album cover Abbey Road, which band member isn\'t wearing shoes?', 
+  a: 'John Lennon',
+  b: 'Ringo Starr',
+  c: 'Paul McCartney',
+  d: 'George Harrison',
+  answer: 'c'
+  },
+];
---
+++ questions/question-set-chlesb.js
@@ -0,0 +1,82 @@
+module.exports = [
+  {
+    question: 'What is ChelsB\'s middle name?', 
+    a: 'Taylor',
+    b: 'Danger',
+    c: 'Anita',
+    d: 'Rebecca',
+    answer: 'd'
+  },
+  {
+    question: 'Which one of these people has shot ChelsB with a paintball gun?', 
+    a: 'Shaun White\'s mom',
+    b: 'Bass player from Foster the People',
+    c: 'Chuck Norris\' Stunt double',
+    d: 'Nick Baldoni',
+    answer: 'b'
+  },
+  {
+    question: 'How many slack emojis has ChelsB created?', 
+    a: '31',
+    b: '71',
+    c: '46',
+    d: '53',
+    answer: 'b'
+  },
+  {
+    question: 'Where has ChelsB not lived?', 
+    a: 'Wisconsin',
+    b: 'Minnesota',
+    c: 'Iowa',
+    d: 'Illinois',
+    answer: 'c'
+  },
+  {
+    question: 'What sport(s) did ChelsB play growing up?', 
+    a: 'Basketball',
+    b: 'Track & Field',
+    c: 'Soccer',
+    d: 'all of the above',
+    answer: 'c'
+  },
+  {
+    question: 'How old is ChelsB?', 
+    a: '25',
+    b: '26',
+    c: '27',
+    d: '28',
+    answer: 'b'
+  },
+  {
+    question: 'Which national health concern first started in ChelsB\'s hometown of Hamilton, NJ?', 
+    a: 'Anthrax',
+    b: 'Bird Flu',
+    c: 'Swine Flu',
+    d: 'Justin Bieber',
+    answer: 'a'
+  },
+  {
+    question: 'ChelsB\'s dog\'s name is Wisco - which is short for what?', 
+    a: 'Whiskey',
+    b: 'Whiskers',
+    c: 'Nothing',
+    d: 'Wisconsin',
+    answer: 'd'
+  },
+  {
+    question: 'ChelsB has eaten fruit from Bill Murray\'s orchard. What kind of fruit was it?', 
+    a: 'Tangleo',
+    b: 'Pomelo',
+    c: 'Lime-elo',
+    d: 'Apple',
+    answer: 'a'
+  },
+  {
+  question: 'Which one of these people has ChelsB seen do a backflip?', 
+  a: 'Shaun White\'s mom',
+  b: 'Bass player from Foster the People',
+  c: 'Chuck Norris\' Stunt double',
+  d: 'Nick Baldoni',
+  answer: 'c'
+  },
+];
