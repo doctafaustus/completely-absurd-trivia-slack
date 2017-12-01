215b5acfb027e45b87afd68d0eb849363f8a5334
doctafaustus <clowd150@gmail.com>
2017-11-30 23:25:07 -0600

    Add Xmas and Marion's Quiz

---
+++ questions/question-set-90s-tv.js
@@ -0,0 +1,82 @@
+module.exports = [
+  {
+    question: 'Which cast member of Zoom was originally born in Russia?', 
+    a: 'Alisa',
+    b: 'Jared',
+    c: 'Zoe',
+    d: 'Lynese',
+    answer: 'a'
+  },
+  {
+    question: 'Which part of NYC does Ghostwriter primarily take place?', 
+    a: 'Manhattan',
+    b: 'Brooklyn',
+    c: 'Queens',
+    d: 'Staten Island',
+    answer: 'b'
+  },
+  {
+    question: 'How did Marty from the ABC show Teen Angel die in the pilot?', 
+    a: 'Fell off a roller coaster',
+    b: 'Ate a 6 month old hamburger',
+    c: 'Had a heart attack during a scary movie',
+    d: 'Died of boredom during detention',
+    answer: 'b'
+  },
+  {
+    question: 'Who is the primary villain in Rescue Rangers?', 
+    a: 'Fat Cat',
+    b: 'Flintheart Glomgold',
+    c: 'Reginald Bushroot',
+    d: 'Banshee',
+    answer: 'a'
+  },
+  {
+    question: 'What was the Animaniacs parody of the tv show Friends called?', 
+    a: 'Best Buddies',
+    b: 'Comrades ',
+    c: 'Acquaintances',
+    d: 'Amigos',
+    answer: 'c'
+  },
+  {
+    question: 'Who sang the theme song of Kenan & Kel?', 
+    a: 'Coolio',
+    b: 'Snoop Dogg',
+    c: 'Busta Rhymes',
+    d: 'Ice Cube',
+    answer: 'a'
+  },
+  {
+    question: 'When did the Adventures of Pete & Pete run on Nickelodeon?', 
+    a: '1990 - 1992',
+    b: '1991 - 1995',
+    c: '1995 - 1997',
+    d: '1993 - 1996',
+    answer: 'd'
+  },
+  {
+    question: 'Who is Sabrinas longterm boyfriend in Sabrina the Teenage Witch?', 
+    a: 'Harvey Kraft',
+    b: 'Harvey Kinkle',
+    c: 'Harvey Dent',
+    d: 'Harvey Keitel',
+    answer: 'b'
+  },
+  {
+    question: 'Which famous Star Wars actor voiced The Joker in Batman the Animated Series?', 
+    a: 'Harrison Ford',
+    b: 'Frank Oz',
+    c: 'Ewan McGregor',
+    d: 'Mark Hamill',
+    answer: 'd'
+  },
+  {
+    question: 'What object is on Chuckie\'s shirt in Rugrats?', 
+    a: 'rocketship',
+    b: 'planet',
+    c: 'crescent moon',
+    d: 'star',
+    answer: 'b'
+  },
+];
---
+++ questions/question-set-christmas.js
@@ -0,0 +1,82 @@
+module.exports = [
+  {
+    question: 'In what decade did Coca-Cola start using Santa Claus in advertisements?',
+    a: '1950s',
+    b: '1900s',
+    c: '1940s',
+    d: '1920s',
+    answer: 'd'
+  },
+  {
+    question: 'Who did the German Nazi Regime replace Santa Claus with?',
+    a: 'Odin',
+    b: 'An SS Soldier',
+    c: 'Hitler',
+    d: 'Jesus',
+    answer: 'a'
+  },
+  {
+    question: 'What popular holiday song was written during the Cuban missile crisis and was a plea for peace?',
+    a: 'Silent Night',
+    b: 'Silver Bells',
+    c: 'Last Christmas',
+    d: 'Do You Hear What I Hear?',
+    answer: 'd'
+  },
+  {
+    question: 'What was the last state to declare Christmas a legal holiday?',
+    a: 'Oklahoma',
+    b: 'Alabama',
+    c: 'California',
+    d: 'Utah',
+    answer: 'a'
+  },
+  {
+    question: 'How many lights are on the Zilker Park Christmas Tree',
+    a: '2568',
+    b: '3309',
+    c: '4877',
+    d: '3905',
+    answer: 'b'
+  },
+  {
+    question: 'Who created the first electric light Christmas display?',
+    a: 'Thomas Edison',
+    b: 'Benjamin Franklin',
+    c: 'Nikola Tesla',
+    d: 'Albert Einstein',
+    answer: 'a'
+  },
+  {
+    question: 'What northeastern US state holds the Guinness record for largest snowman?',
+    a: 'New York',
+    b: 'New Hampshire',
+    c: 'Pennsylvania',
+    d: 'Maine',
+    answer: 'd'
+  },
+  {
+    question: 'What country is the Poinsettia, with its red and green foliage, native to?',
+    a: 'Brazil',
+    b: 'Mexico',
+    c: 'Chile',
+    d: 'Costa Rica',
+    answer: 'b'
+  },
+  {
+    question: 'What large former retail outlet commissioned and published Rudolph the Red-Nosed Reindeer?',
+    a: 'Sears',
+    b: 'Foley\'s',
+    c: 'Montgomery Ward',
+    d: 'Woolworth',
+    answer: 'c'
+  },
+  {
+    question: 'What is the most popular meal for Christmas in Japan?',
+    a: 'McDonald\'s Filet-O-Fish',
+    b: 'KFC Chicken',
+    c: 'Domino\'s Mayo Patato Pizza',
+    d: 'Burger King Garlic Meat Beast Burger',
+    answer: 'b'
+  },
+];
