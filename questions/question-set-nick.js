5d03e5523f70c9ebf9325817ad2863dc047f21b4
doctafaustus <clowd150@gmail.com>
2017-12-07 10:13:54 -0600

    Add and configure 'How Well Do You Know the Design Team'

---
+++ questions/question-set-nick.js
@@ -0,0 +1,82 @@
+module.exports = [
+  {
+    question: 'Which designer has lived in Alaska?', 
+    a: 'Haley',
+    b: 'Sean',
+    c: 'Nick',
+    d: 'Darice',
+    answer: 'c'
+  },
+  {
+    question: 'Which designer compared the responsibility of buying a coat to buying a house?', 
+    a: 'Chelsb',
+    b: 'Mitch',
+    c: 'Carolyn',
+    d: 'Becky',
+    answer: 'd'
+  },
+  {
+    question: 'Which designer is a closet rock climbing pro?', 
+    a: 'Autumn',
+    b: 'Sean',
+    c: 'West',
+    d: 'Nick',
+    answer: 'a'
+  },
+  {
+    question: 'Which designer is a member of a royal bloodline?', 
+    a: 'Haley',
+    b: 'John',
+    c: 'Couv',
+    d: 'Libby',
+    answer: 'b'
+  },
+  {
+    question: 'Darice has two shiba inus! What are their names?', 
+    a: 'Salt and Pepper',
+    b: 'Bubba and Hoss',
+    c: 'Shadow and Luna',
+    d: 'Whiskers and Potato',
+    answer: 'c'
+  },
+  {
+    question: 'Which designer has a habit of ~stealing~ rescuing plants, and growing them at home?', 
+    a: 'Darice',
+    b: 'Jessica',
+    c: 'West',
+    d: 'Becky',
+    answer: 'c'
+  },
+  {
+    question: 'Which designer used to be a science teacher?', 
+    a: 'John',
+    b: 'Libby',
+    c: 'Mitch',
+    d: 'Couv',
+    answer: 'a'
+  },
+  {
+    question: 'Which designer orders iced coffee, minus the coffee, when they go to Jos?', 
+    a: 'Haley',
+    b: 'Libby',
+    c: 'Sean',
+    d: 'Jessica',
+    answer: 'b'
+  },
+  {
+    question: 'Which designer hates mashed potatoes?', 
+    a: 'Autumn',
+    b: 'Carolyn',
+    c: 'Chelsb',
+    d: 'Mitch',
+    answer: 'c'
+  },
+  {
+    question: 'What is a group designers known as collectively?', 
+    a: 'A squad',
+    b: 'A team',
+    c: 'A flock',
+    d: 'A cult',
+    answer: 'd'
+  },
+];
