cb59be03190434623e21fddae9b58049aa7d3bfc
doctafaustus <clowd150@gmail.com>
2017-12-08 01:09:35 -0600

    Add question set 24

---
+++ questions/question-set-24.js
@@ -0,0 +1,82 @@
+module.exports = [
+  {
+    question: 'What is the official name of Trader Joe\'s "Two Buck Chuck" wine?', 
+    a: 'Charles Shaw Wine',
+    b: 'Charles Bronson Wine',
+    c: 'Charlie Buck Wine',
+    d: 'Chuck Liddel Wine',
+    answer: 'a'
+  },
+  {
+    question: 'Which street in New York City is ranked among the most expensive retail shopping streets in the world?', 
+    a: '5th Avenue',
+    b: '7th Avenue',
+    c: 'Times Square',
+    d: 'Broadway',
+    answer: 'a'
+  },
+  {
+    question: 'What is the second largest country in the world (by area)?', 
+    a: 'Russia',
+    b: 'Canada',
+    c: 'China',
+    d: 'Brazil',
+    answer: 'b'
+  },
+  {
+    question: 'Who founded the Church of Scientology?', 
+    a: 'Albert Benjamin Simpson',
+    b: 'Jakob Ammann',
+    c: 'L. Ron Hubbard',
+    d: 'Joseph Smith',
+    answer: 'c'
+  },
+  {
+    question: 'A musical recording that contains more tracks than a single, but not enough to qualify as an album is a(n) ____ record.', 
+    a: 'LP',
+    b: 'EP',
+    c: 'CD',
+    d: 'LD',
+    answer: 'b'
+  },
+  {
+    question: 'What was the name of the first video ever uploaded to YouTube?', 
+    a: 'Cute Cats',
+    b: 'Me at the Zoo',
+    c: 'Flambe Disaster',
+    d: 'Hello World',
+    answer: 'b'
+  },
+  {
+    question: 'Android OS versions are code-named after what?', 
+    a: 'Toy Story Characters',
+    b: 'Animals',
+    c: 'Mountains',
+    d: 'Confections',
+    answer: 'd'
+  },
+  {
+    question: 'The the accumulation of gas in the alimentary canal is also known as ____.', 
+    a: 'Blockchain',
+    b: 'Flatulence',
+    c: 'Combustion',
+    d: 'Attenuation ',
+    answer: 'b'
+  },
+  {
+    question: 'What is "Turkish Delight"?', 
+    a: 'A Dessert',
+    b: 'A Dance',
+    c: 'A Plant',
+    d: 'A Play',
+    answer: 'a'
+  },
+  {
+    question: 'What was the name of the first cloned mammal?', 
+    a: 'Heidi',
+    b: 'Harambe',
+    c: 'Bo',
+    d: 'Dolly',
+    answer: 'd'
+  },
+];
