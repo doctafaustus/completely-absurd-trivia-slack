15d504a72777eeffe044d0cecf6dd09d741c7562
doctafaustus <clowd150@gmail.com>
2017-12-08 13:18:11 -0600

    Add question set Dante

---
+++ questions/question-set-dante.js
@@ -0,0 +1,82 @@
+module.exports = [
+  {
+    question: 'Within Accenture, what does CAL stand for?',
+    a: 'Customer Acquisition Lead',
+    b: 'Client Account Lead',
+    c: 'Committee Accord Leaflet',
+    d: 'Customer Action Legend',
+    answer: 'b'
+  },
+  {
+    question: 'In Clearhead analytics, what does MDE mean?',
+    a: 'Minimum Decrease Equation',
+    b: 'Maximum Detectable Effect',
+    c: 'Minimum Detectable Effect',
+    d: 'Maximum Determinable Evaluation',
+    answer: 'c'
+  },
+  {
+    question: 'In business, EPS is shorthand for what?',
+    a: 'Earnings Per Share',
+    b: 'Earnings Per Solution',
+    c: 'Employees Per State',
+    d: 'Employees Per Sector',
+    answer: 'a'
+  },
+  {
+   question: 'In computer software, what does API stand for?',
+    a: 'Application Program Interface',
+    b: 'Application Peer Interaction',
+    c: 'Application Program Interactions',
+    d: 'Apparel Program Indicator',
+    answer: 'a'
+  },
+  {
+    question: 'What tool does MME stand for in the Accenture world?',
+    a: 'Manage My Expenses',
+    b: 'Manage My Emotions',
+    c: 'Manage My Engagements',
+    d: 'Merge My Expenses',
+    answer: 'c'
+  },
+  {
+    question: 'On the internet, HTTP stands for ____.',
+    a: 'Hypertext Tool Protocol',
+    b: 'Hypertext Transfer Protocol',
+    c: 'Highway Text Tool Protocol',
+    d: 'Highfrequency Text Transfer Program',
+    answer: 'b'
+  },
+  {
+    question: 'In marketing, CRM is the acronym for what?',
+    a: 'Customer Relationship Management',
+    b: 'Content Record Management',
+    c: 'Consumer Relationship Manager',
+    d: 'Customer Record Merger',
+    answer: 'a'
+  },
+  {
+    question: 'In Accenture, MYTE is shorthand for what tool?',
+    a: 'My Time is Everything',
+    b: 'My Time and Expenses',
+    c: 'My Tits and Ass',
+    d: 'My Time and Experience',
+    answer: 'b'
+  },
+  {
+    question: 'In the Internets, URL is short for what?',
+    a: 'Universal Record Locator',
+    b: 'Universal Record Legend',
+    c: 'Universal Resource Locator',
+    d: 'Universe Resource Legend',
+    answer: 'c'
+  },
+  {
+    question: 'In computer software, RAM is an acronym for what?',
+    a: 'Results At Moments Notice',
+    b: 'Rich Access Memory',
+    c: 'Random Access Memory',
+    d: 'Rich Access Mechanism',
+    answer: 'c'
+  }
+];
