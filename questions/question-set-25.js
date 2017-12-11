03dd82c2d3bff5f3ec465524e813a17abd6627e1
doctafaustus <clowd150@gmail.com>
2017-12-10 20:48:39 -0600

    Add question set 25

---
+++ questions/question-set-25.js
@@ -0,0 +1,82 @@
+module.exports = [
+  {
+    question: 'Which medication is an antihistamine?', 
+    a: 'Lamisil',
+    b: 'Tylenol',
+    c: 'Benadryl',
+    d: 'Motrin',
+    answer: 'c'
+  },
+  {
+    question: 'The vertical bar key on a keyboard, "|" is also referred to as what?', 
+    a: 'Pipe Symbol',
+    b: 'Harp Key',
+    c: 'Bar Symbol',
+    d: 'Straight Slash Key',
+    answer: 'a'
+  },
+  {
+    question: 'What of the following is NOT a component of a toilet?', 
+    a: 'Flapper',
+    b: 'Float',
+    c: 'Fill Valve',
+    d: 'Tub Ring',
+    answer: 'd'
+  },
+  {
+    question: 'What kind of creature was Gizmo from the move "Gremlims"?', 
+    a: 'Gelfling',
+    b: 'Mogwai',
+    c: 'Na\'vi',
+    d: 'Zokzah',
+    answer: 'b'
+  },
+  {
+    question: 'Which of the following animals is a carnivore?', 
+    a: 'Whale',
+    b: 'Deer',
+    c: 'Chimpanzee',
+    d: 'Caterpillar',
+    answer: 'a'
+  },
+  {
+    question: 'What is Luigi\'s last name from the Mario Bros. video game franchise?' , 
+    a: 'Miyamoto',
+    b: 'Mario',
+    c: 'DiNapoli',
+    d: 'Esposito',
+    answer: 'b'
+  },
+  {
+    question: 'Where was pizza invented?', 
+    a: 'Bronx, New York',
+    b: 'Brooklyn, New York',
+    c: 'Florence, Italy',
+    d: 'Naples, Italy',
+    answer: 'd'
+  },
+  {
+    question: 'President Clinton and his family had a pet named Socks. What kind of pet was it?', 
+    a: 'Ferret',
+    b: 'Cat',
+    c: 'Rabbit',
+    d: 'Dog',
+    answer: 'b'
+  },
+  {
+    question: 'Which part of the plant/tree is cinnamon made out of?', 
+    a: 'The Flower',
+    b: 'The Stem',
+    c: 'The Root',
+    d: 'The Bark',
+    answer: 'd'
+  },
+  {
+    question: 'Which item is seen in the famous painting "The Birth of Venus"?', 
+    a: 'Scallop Shell',
+    b: 'Harp',
+    c: 'Serpent',
+    d: 'White Horse',
+    answer: 'a'
+  },
+];
