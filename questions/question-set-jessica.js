d76926b8b9df0c3ee7650a619753f3bed82653a0
doctafaustus <clowd150@gmail.com>
2017-12-12 12:15:57 -0600

    Add question set Jessica

---
+++ questions/question-set-jessica.js
@@ -0,0 +1,82 @@
+module.exports = [
+ {
+   question: 'Which pair of colors are complimentary?',
+   a: 'Red and blue',
+   b: 'Red and orange',
+   c: 'Purple and orange',
+   d: 'Purple and yellow',
+   answer: 'd'
+ },
+ {
+   question: 'What year was Toy Story released?',
+   a: '1995',
+   b: '1996',
+   c: '1997',
+   d: '1998',
+   answer: 'a'
+ },
+ {
+   question: 'What century was the Mona Lisa painted in?',
+   a: '15th Century',
+   b: '16th Century',
+   c: '17th Century',
+   d: '18th Century',
+   answer: 'b'
+ },
+ {
+   question: 'Van Gogh painted for only 9 years of his life. How many artworks did he create in that time?',
+   a: '450',
+   b: '860',
+   c: '1,550',
+   d: '2,100',
+   answer: 'd'
+ },
+ {
+   question: 'Which famous person did Annie Leibovitz photograph the day that he was later killed?',
+   a: 'Steve McQueen',
+   b: 'Selena',
+   c: 'John Lennon',
+   d: 'Biggie Smalls',
+   answer: 'c'
+ },
+ {
+   question: 'Who is the author and illustrator of Where the Wild Things Are?',
+   a: 'Eric Carle',
+   b: 'Maurice Sendak',
+   c: 'Ludwig Bemelmans',
+   d: 'Shel Silverstein',
+   answer: 'b'
+ },
+ {
+   question: 'Beyonce is the most grammy-nominated woman in history. How many noms has she received?',
+   a: '37',
+   b: '48',
+   c: '63',
+   d: '71',
+   answer: 'c'
+ },
+ {
+   question: 'Which color does the human eye process first?',
+   a: 'Yellow',
+   b: 'Green',
+   c: 'Blue',
+   d: 'Red',
+   answer: 'a'
+ },
+ {
+   question: 'What were the colors of the Women\'s Suffrage movement?',
+   a: 'Purple, Yellow & White',
+   b: 'Purple, Green & White',
+   c: 'Purple, Pink & Green',
+   d: 'Purple, Yellow & Green',
+   answer: 'b'
+ },
+ {
+   question: 'What is the name of Tina Belcher\'s vocal group in Bob\'s Burgers?',
+   a: 'The Shirley Tempos',
+   b: 'The Hormone-iums',
+   c: 'Pitch Slapped',
+   d: 'The Unaccompanied Minors',
+   answer: 'b'
+ },
+];
