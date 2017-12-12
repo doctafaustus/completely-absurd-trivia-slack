ebea05b6408b9536ecb21045815445bc48507077
doctafaustus <clowd150@gmail.com>
2017-12-12 14:47:05 -0600

    Add question set James

---
+++ questions/question-set-james.js
@@ -0,0 +1,82 @@
+module.exports = [
+  {
+    question: 'Before he was breaking the hearts of middle-aged women across the city, what band was Bob Schneider in?', 
+    a: 'The Scabs',
+    b: 'The Scabies',
+    c: 'The Stokes',
+    d: 'The Sting',
+    answer: 'a'
+  },
+  {
+    question: 'What famous musician from the 60s/70s used to play weekly gigs with their pals at Threadgills?', 
+    a: 'Willie Nelson',
+    b: 'Stevie Ray Vaughn',
+    c: 'Janis Joplin',
+    d: 'Jimmie Vaughn',
+    answer: 'c'
+  },
+  {
+    question: 'What famous music venue has two stages and doubles as an equally famous BBQ spot?', 
+    a: 'Lamberts BBQ',
+    b: 'Sams BBQ',
+    c: 'Franklins BBQ',
+    d: 'Stubbs BBQ',
+    answer: 'd'
+  },
+  {
+    question: 'What company puts on Austin City Limits Music Festival?', 
+    a: 'C3 Presents',
+    b: 'C3 Concerts',
+    c: 'C3 Festivals',
+    d: 'Austin City Limits, LLC',
+    answer: 'a'
+  },
+  {
+    question: 'There is a venue on Red River called The Sidewinder. What was the name before it was called The Sidewinder?', 
+    a: 'Red 7',
+    b: 'Red Eyed Fly',
+    c: 'Reds Porch',
+    d: 'Reds Indoor Range',
+    answer: 'b'
+  },
+  {
+    question: 'According to the Austin Music Awards, who was the best Blues/Soul/Funk act of 2016-2017?', 
+    a: 'Gary Clark, Jr.',
+    b: 'Alesia Lani',
+    c: 'Peterson Brothers',
+    d: 'Shiny Ribs',
+    answer: 'd'
+  },
+  {
+    question: 'What is Shiny Ribs given name?', 
+    a: 'Russell Simmons',
+    b: 'Richard Simmons',
+    c: 'Kevin Russell',
+    d: 'Kevin Richards',
+    answer: 'c'
+  },
+  {
+    question: 'What band gained national recognition after being heavily featured on the famous movie & TV show Friday Night Lights?', 
+    a: 'This Will Destroy You',
+    b: 'Explosions in the Sky',
+    c: 'God Speed You Black Emperor',
+    d: 'Mogwai',
+    answer: 'b'
+  },
+  {
+    question: 'What is the name of the lead singer in the band Spoon?', 
+    a: 'Brett Daniels',
+    b: 'Britt Daniel',
+    c: 'Brett David',
+    d: 'Britt Davidson',
+    answer: 'b'
+  },
+  {
+    question: 'What Austin band hit top five in the charts in 1998 with their hit "The Way"?', 
+    a: 'Sugar Ray',
+    b: 'Pearl Jammies',
+    c: 'Nerve Ana',
+    d: 'Fastball',
+    answer: 'd'
+  },
+];
