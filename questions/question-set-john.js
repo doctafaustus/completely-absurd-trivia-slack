9aa81d7c92978fb27ebbb46639701602162f7b62
doctafaustus <clowd150@gmail.com>
2017-12-12 13:24:39 -0600

    Add question set John

---
+++ questions/question-set-john.js
@@ -0,0 +1,82 @@
+module.exports = [
+ {
+   question: 'Which of the following properties are measured in Pascals?',
+   a: 'Force',
+   b: 'Volume',
+   c: 'Pressure',
+   d: 'Viscosity',
+   answer: 'c'
+ },
+ {
+   question: 'What element has the symbol "Co"?',
+   a: 'Copper',
+   b: 'Chromium',
+   c: 'Copernicum',
+   d: 'Cobalt',
+   answer: 'd'
+ },
+ {
+   question: 'Which scientist is the only person to win Nobel prizes in two different sciences?',
+   a: 'Werner Heisenberg',
+   b: 'Marie Curie',
+   c: 'Nikola Tesla',
+   d: 'Rosalind Franklin',
+   answer: 'b'
+ },
+ {
+   question: 'What freezes faster: hot water or cold water?',
+   a: 'Hot Water',
+   b: 'Cold Water',
+   c: 'Both Freeze at the Same Rate',
+   d: 'The Rate Depends on Location',
+   answer: 'a'
+ },
+ {
+   question: 'After distilling his own urine in attempt to create the philosopher\'s stone, what element did Hennig Brand accidentally discover?',
+   a: 'Magnesium',
+   b: 'Sulfur',
+   c: 'Sodium',
+   d: 'Phosphorus',
+   answer: 'd'
+ },
+ {
+   question: 'What compound is the active component in chili peppers?',
+   a: 'Lutein',
+   b: 'Capsaicin',
+   c: 'Acrylamide',
+   d: 'Pyrazine',
+   answer: 'b'
+ },
+ {
+   question: 'What kind of technique was used to analyze the composition of soil samples on Mars?',
+   a: 'Calorimetry',
+   b: 'Chromatography',
+   c: 'Isotope Dilution',
+   d: 'Spectrometry',
+   answer: 'd'
+ },
+ {
+   question: 'Which of the following liquids is the most dense?',
+   a: 'Water',
+   b: 'Gasoline',
+   c: 'Acetone',
+   d: 'Chloroform',
+   answer: 'd'
+ },
+ {
+   question: 'In 1901, a dental school graduate moved to Colorado Springs and discovered that all of the residents had brown teeth because of this element:',
+   a: 'Fluorine',
+   b: 'Magnesium',
+   c: 'Chlorine',
+   d: 'Strontium',
+   answer: 'a'
+ },
+ {
+   question: 'What chemical property is a measurement of disorder in a system?',
+   a: 'Enthalpy',
+   b: 'Gibbs Free Energy',
+   c: 'Entropy',
+   d: 'Equilibrium Constant',
+   answer: 'c'
+ },
+];
