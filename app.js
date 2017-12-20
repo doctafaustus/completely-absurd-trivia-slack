72e8c7da40a5fdfe53c29253700d02da58dd5a3c
doctafaustus <clowd150@gmail.com>
2017-12-20 12:54:45 -0600

    Configure question set Adam

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ var prodRealURL = 'https://hooks.slack.com/services/T02A4DYBJ/B7WHHJC05/2lPlwdnh
 
 var webhookURL = prodRealURL;
 
-var questions = require('./questions/question-set-john');
+var questions = require('./questions/question-set-adam');
 
 
 // Trivia database API
