17acca425a90be71ca898f2c06620f069774c991
doctafaustus <clowd150@gmail.com>
2017-12-20 14:35:30 -0600

    Add question set Marfa

---
+++ questions/question-set-marfa.js
@@ -0,0 +1,82 @@
+module.exports = [
+  {
+    question: 'Who was voted Most Likely To Try To Keep The Party From Dying?', 
+    a: 'BDF',
+    b: 'Easy-E',
+    c: 'Billy',
+    d: 'RG',
+    answer: 'd'
+  },
+  {
+    question: 'Who very unsuccessfully tried to open a bottle of Rose w/o a bottle opener?', 
+    a: 'J Lowe',
+    b: 'Haley',
+    c: 'Dante',
+    d: 'Josh Payne',
+    answer: 'a'
+  },
+  {
+    question: 'Who read people\'s Tinder profiles in a British accent?', 
+    a: 'J Lowe',
+    b: 'Becky',
+    c: 'Couv',
+    d: 'Ade',
+    answer: 'd'
+  },
+    {
+    question: 'Who\'s teepee turned into a dance party?', 
+    a: 'Joco',
+    b: 'Jessica',
+    c: 'Shanwow',
+    d: 'Quincy',
+    answer: 'c'
+  },
+  {
+    question: 'Who said, "to everyone that doubted me, bye-bye haters."?', 
+    a: 'Haley',
+    b: 'Dante',
+    c: 'J Lowe',
+    d: 'RG',
+    answer: 'c'
+  },
+  {
+    question: 'Who said, "Let\'s get hard!... WHAT? I\'M ALREADY HARD"?', 
+    a: 'Starchy',
+    b: 'Mark Perica',
+    c: 'Becky',
+    d: 'J Lowe',
+    answer: 'c'
+  },
+  {
+    question: 'What was voted favorite Wumba (water zumba) move?', 
+    a: 'The Twirl',
+    b: 'Double Lassos',
+    c: 'The Shimmy',
+    d: 'The arms extended above your head, then rolling down the bottom, like "damn, look at what I\'ve got." Not sure what to call that.',
+    answer: 'a'
+  },
+  {
+    question: 'What was voted Favorite Campfire Song?', 
+    a: 'Wonderwall',
+    b: 'Jane (Degen) Says', 
+    c: 'What\'s Up - 4 non-blondes',
+    d: 'All 7 renditions of Say It Ain\'t So',
+    answer: 'b'
+  },
+  {
+    question: 'Who was voted First to Start AND Last to Stop Drinking AND Most Likely to Take the Last Can?', 
+    a: 'BDF',
+    b: 'Easy E',
+    c: 'Jeff',
+    d: 'Joco',
+    answer: 'b'
+  },
+  {
+    question: 'Who farted during Crossfit?', 
+    a: 'J Lowe',
+    b: 'RG',
+    c: 'JB',
+    d: 'Brady',
+    answer: 'b'
+  },
+];
