ae8c1961b30d15a04af362d7cadd28640baccba2
doctafaustus <clowd150@gmail.com>
2018-03-02 16:49:53 -0600

    Add and configure question-set-27

---
+++ questions/question-set-27.js
@@ -0,0 +1,82 @@
+module.exports = [
+  {
+    question: 'Which of these inventions was NOT showcased at the 1893 Chicago World\'s Fair',
+    a: 'Hair Dryer',
+    b: 'Spray Paint',
+    c: 'Dishwasher',
+    d: 'Pabst Blue Ribbon',
+    answer: 'a'
+  },
+  {
+    question: 'What is the largest living carnivore?',
+    a: 'Hippo',
+    b: 'Grizzly Bear',
+    c: 'Great White Shark',
+    d: 'Elephant Seal',
+    answer: 'd'
+  },
+  {
+    question: 'How much money does an US Olympic Gold Medal winner get?',
+    a: '$10k',
+    b: '$25k',
+    c: '$50k',
+    d: '$75k',
+    answer: 'b'
+  },
+  {
+    question: 'What movie was called "Run! Run! CLOUDZILLA!" in China?',
+    a: 'Cloudy With a Chance of Meatballs',
+    b: 'The Mist',
+    c: 'Twister',
+    d: 'Sharknado',
+    answer: 'c'
+  },
+  {
+    question: 'In what key do houseflies hum?',
+    a: 'D',
+    b: 'E',
+    c: 'F',
+    d: 'G',
+    answer: 'c'
+  },
+  {
+    question: 'What is the best selling Nintendo game of all time?',
+    a: 'Super Mario Bros',
+    b: 'Wii Sports',
+    c: 'Mario Kart',
+    d: 'Legend of Zelda',
+    answer: 'b'
+  },
+  {
+    question: 'What does the X in X-Men stand for?',
+    a: 'Professor X',
+    b: 'Exceptional',
+    c: 'Extreme',
+    d: 'Extra',
+    answer: 'd'
+  },
+  {
+    question: 'What group of animals is officially called an "Army"?',
+    a: 'Caterpillars',
+    b: 'Ants',
+    c: 'Wombats',
+    d: 'Mice',
+    answer: 'a'
+  },
+  {
+    question: 'What has the highest pH level?',
+    a: 'Vinegar',
+    b: 'Water',
+    c: 'Lemon Juice',
+    d: 'Orange Juice',
+    answer: 'b'
+  },
+  {
+    question: 'Who coined the term "White House"?',
+    a: 'Abraham Lincoln',
+    b: 'Benjamin Franklin',
+    c: 'Teddy Roosevelt',
+    d: 'Grover Cleveland',
+    answer: 'c'
+  },
+];
