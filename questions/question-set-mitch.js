2dce3a9fed1d505eaa61a4b0e29e14ba0e7fbd04
doctafaustus <clowd150@gmail.com>
2018-04-26 16:54:10 -0500

    Add quesiton set mitch

---
+++ questions/question-set-mitch.js
@@ -0,0 +1,82 @@
+module.exports = [
+ {
+   question: 'How much money has the "Marvel Cinematic Universe" grossed since 2008?',
+   a: '$4.21 Billion',
+   b: '$4.66 Billion',
+   c: '$5.96 Billion',
+   d: '$6.82 Billion',
+   answer: 'c'
+ },
+ {
+   question: 'What was the original name of Marvel Comics?',
+   a: 'Timely Publications',
+   b: 'Marvelous Comics',
+   c: 'Fantastic Adventures',
+   d: 'National Allied Publications',
+   answer: 'a'
+ },
+ {
+   question: 'What is the most expensive film ever made?',
+   a: 'Avengers: Infinity War',
+   b: 'Pirates of the Caribbean: On Stranger Tides',
+   c: 'Justice League',
+   d: 'Harry Potter and the Half-Blood Prince',
+   answer: 'b'
+ },
+ {
+   question: 'What is Stan Lee\'s birth name?',
+   a: 'Stanley Lee',
+   b: 'Stanley Leibowitz',
+   c: 'Stanley Laurence',
+   d: 'Stanley Lieber',
+   answer: 'd'
+ },
+ {
+   question: 'What is the worst rated comic book movie, according to Rotten Tomatoes?',
+   a: 'The Crow: Wicked Prayer',
+   b: 'I Frankenstein',
+   c: 'Catwoman',
+   d: 'Batman vs. Superman: Dawn of Justice',
+   answer: 'a'
+ },
+ {
+   question: 'How many times has Robert Downey Jr. played Iron Man?',
+   a: '10',
+   b: '9',
+   c: '8',
+   d: '7',
+   answer: 'b'
+ },
+ {
+   question: 'Which character is not owned by Marvel Studios?',
+   a: 'Captain Marvel',
+   b: 'Green Lantern',
+   c: 'Elektra',
+   d: 'Hank Pym',
+   answer: 'b'
+ },
+ {
+   question: 'Which actor is not currently part of the Marvel Cinematic Universe?',
+   a: 'William Hurt',
+   b: 'Martin Freeman',
+   c: 'Felicity Jones',
+   d: 'Evangeline Lilly',
+   answer: 'c'
+ },
+ {
+   question: 'Which character does Idris Elba play in the Marvel movies?',
+   a: 'Heimdall',
+   b: 'Tanngrisnir',
+   c: 'Loki',
+   d: 'Donar',
+   answer: 'a'
+ },
+ {
+   question: 'How old is Chris Pratt?',
+   a: '39',
+   b: '40',
+   c: '35',
+   d: '38',
+   answer: 'd'
+ },
+];
