e6513602e872196cdba86bcf4ccdb61c99577d4a
doctafaustus <clowd150@gmail.com>
2018-05-04 15:09:09 -0500

    Add question set james-spelling and configure question set 29

---
+++ questions/question-set-james-spelling.js
@@ -0,0 +1,82 @@
+module.exports = [
+  {
+    question: 'How do you spell what drummers have?', 
+    a: 'Rhythm',
+    b: 'Rhythym',
+    c: 'Rythm',
+    d: 'Rythym',
+    answer: 'a'
+  },
+  {
+    question: 'How do you spell something that is required to be done?', 
+    a: 'Necessary',
+    b: 'Neccessary',
+    c: 'Necesary',
+    d: 'Necissary',
+    answer: 'a'
+  },
+  {
+    question: 'How do you spell what Alfred Yankovic is?', 
+    a: 'Weerd',
+    b: 'Weird',
+    c: 'Wierd',
+    d: 'Weered',
+    answer: 'b'
+  },
+  {
+    question: 'How do you spell this place where we get buried?', 
+    a: 'Cemetary',
+    b: 'Simitary',
+    c: 'Cimitary',
+    d: 'Cemetery',
+    answer: 'd'
+  },
+  {
+    question: 'How do you spell the feeling you get when you check in late to trivia?', 
+    a: 'Embarassed',
+    b: 'Embaressed',
+    c: 'Embarrassed',
+    d: 'Embearessed',
+    answer: 'c'
+  },
+  {
+    question: 'How do you spell where Johnny Depp is a pirate?', 
+    a: 'Caribbean',
+    b: 'Caribean',
+    c: 'Carribean',
+    d: 'Carribbean',
+    answer: 'a'
+  },
+  {
+    question: 'How do you ____________ spelling this word?', 
+    a: 'Recomend',
+    b: 'Recommend',
+    c: 'Reccommend',
+    d: 'Reccomend',
+    answer: 'b'
+  },
+  {
+    question: 'How do you spell the thing that takes the place of something else?', 
+    a: 'Supercede',
+    b: 'Suprecede',
+    c: 'Supreseed',
+    d: 'Supersede',
+    answer: 'd'
+  },
+  {
+    question: 'How do you spell what Jiminy Cricket is to Pinocchio?', 
+    a: 'Conscience',
+    b: 'Consciense',
+    c: 'Conscence',
+    d: 'Conscious',
+    answer: 'a'
+  },
+  {
+    question: 'How do you spell what Robert Mueller is really good at doing?', 
+    a: 'Indict',
+    b: 'Endict',
+    c: 'Indite',
+    d: 'Endite',
+    answer: 'a'
+  },
+];
