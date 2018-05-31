c3ace4d40dbba607b838f416e14be417b0511b42
doctafaustus <clowd150@gmail.com>
2018-05-30 23:52:08 -0500

    Add and configure question set 30

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ var prodRealURL = 'https://hooks.slack.com/services/T02A4DYBJ/B7WHHJC05/2lPlwdnh
 
 var webhookURL = prodRealURL;
 
-var questions = require('./questions/question-set-29');
+var questions = require('./questions/question-set-30');
 
 
 
---
+++ questions/question-set-30.js
@@ -0,0 +1,82 @@
+module.exports = [
+  {
+    question: 'Who composed the 1998 hit song "Blue (Da Ba Dee)"?', 
+    a: 'Tal Bachman',
+    b: 'Blink 182',
+    c: 'Eiffel 65',
+    d: 'Paris 35',
+    answer: 'c'
+  },
+  {
+    question: 'Which of the following is a popular Filipino fast food franchise?', 
+    a: 'Tim Hortons',
+    b: 'Jollibee',
+    c: 'Yo! Sushi',
+    d: 'FEBO',
+    answer: 'b'
+  },
+  {
+    question: 'What city in Colorado was named after a valuable ore compound?', 
+    a: 'Telluride',
+    b: 'Granada',
+    c: 'La Veta',
+    d: 'Castle Rock',
+    answer: 'a'
+  },
+  {
+    question: 'Which of these business is NOT owned by Amazon?', 
+    a: 'Zappos',
+    b: 'Audible',
+    c: 'IMDB',
+    d: 'Hulu',
+    answer: 'd'
+  },
+  {
+    question: 'Who played Mary Katherine Gallagher in the film "Superstar"?', 
+    a: 'Tina Fey',
+    b: 'Rachel Dratch',
+    c: 'Molly Shannon',
+    d: 'Amy Poehler',
+    answer: 'c'
+  },
+  {
+    question: 'Entomology is the study of what?', 
+    a: 'Insects',
+    b: 'Words',
+    c: 'Ocean',
+    d: 'Trees',
+    answer: 'a'
+  },
+  {
+    question: 'What weapon does Raphael, the Ninja Turtle, use?', 
+    a: 'Sword',
+    b: 'Nunchuck',
+    c: 'Sai',
+    d: 'Bo',
+    answer: 'c'
+  },
+  {
+    question: 'What tool is also known as a "hex key"?', 
+    a: 'Allen Wrench',
+    b: 'Torque Wrench',
+    c: 'Gavel',
+    d: 'Drill Bit',
+    answer: 'a'
+  },
+  {
+    question: 'Hannibal\'s army famously crossed the Alps riding what animal?', 
+    a: 'Yaks',
+    b: 'Elephants',
+    c: 'Donkeys',
+    d: 'Mules',
+    answer: 'b'
+  },
+  {
+    question: 'How many months of the year have 30 days?', 
+    a: 'Four',
+    b: 'Five',
+    c: 'Six',
+    d: 'Seven',
+    answer: 'a'
+  },
+];
