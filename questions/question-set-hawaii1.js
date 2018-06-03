289450da94f41add6feabf9eaa08feae4458257e
doctafaustus <clowd150@gmail.com>
2018-06-03 15:16:39 -0500

    Add question set Hawaii 1 & 2

---
+++ questions/question-set-hawaii1.js
@@ -0,0 +1,82 @@
+module.exports = [
+  {
+    question: 'What is the official Hawaiian state flower?', 
+    a: 'Camellia',
+    b: 'Yellow hibiscus',
+    c: 'Pasque flower',
+    d: 'Pink lady\'s slipper',
+    answer: 'b'
+  },
+  {
+    question: 'What is the tallest mountain in Hawaii?', 
+    a: 'Diamond Head',
+    b: 'Mauna Loa',
+    c: 'Ko\'olau Range',
+    d: 'Mauna Kea',
+    answer: 'd'
+  },
+  {
+    question: 'Wreath of flowers presented when arriving or leaving Hawaii?', 
+    a: 'Haku',
+    b: 'Hilo',
+    c: 'Kui',
+    d: 'Lei',
+    answer: 'd'
+  },
+  {
+    question: 'Hawaii\'s capital city is located on which island?', 
+    a: 'Maui',
+    b: 'Moloka\'i',
+    c: 'Oahu',
+    d: 'Island of Hawaii (The Big Island)',
+    answer: 'c'
+  },
+  {
+    question: 'In agriculture, Hawaii is the only state that grows _____', 
+    a: 'Sugar Cane',
+    b: 'Pineapple',
+    c: 'Kiwi Fruit',
+    d: 'Coffee',
+    answer: 'd'
+  },
+  {
+    question: 'Hawaii\'s official nickname is _____', 
+    a: 'Pacific Paradise',
+    b: 'The Lei State',
+    c: 'The Aloha State',
+    d: 'White Sands Beach',
+    answer: 'c'
+  },
+  {
+    question: 'The official Hawaiian fish is called _____', 
+    a: 'rectangular triggerfish',
+    b: 'Humuhumunukunukuapua\'a',
+    c: 'Hawaiian Reef Triggerfish',
+    d: 'All of the above',
+    answer: 'd'
+  },
+  {
+    question: 'Which food is NOT a traditional luau dish?', 
+    a: 'Kalua Pork',
+    b: 'Lomilomi Salmon',
+    c: 'Ajiaco',
+    d: 'Poi',
+    answer: 'c'
+  },
+  {
+    question: 'What is Hawaii\'s all-time record low temperature?', 
+    a: '50 F',
+    b: '42 F',
+    c: '12 F',
+    d: '7 F',
+    answer: 'c'
+  },
+  {
+    question: 'The Hawaii Volcanoes National Park includes with active volcano?', 
+    a: 'Kaho\'olawe',
+    b: 'Kilauea',
+    c: 'Hale\'akala',
+    d: 'Hualalai',
+    answer: 'b'
+  },
+];
---
+++ questions/question-set-hawaii2.js
@@ -0,0 +1,82 @@
+module.exports = [
+  {
+    question: 'When was the surprise attack by Japan on Pearl Harbor?', 
+    a: 'December 7 1941',
+    b: 'December 14 1941',
+    c: 'December 7 1940',
+    d: 'December 14 1942',
+    answer: 'a'
+  },
+  {
+    question: 'What is the official Hawaiian state team sport?', 
+    a: 'Baseball',
+    b: 'Soccer',
+    c: 'Outrigger canoe paddling',
+    d: 'Beach Volleyball',
+    answer: 'c'
+  },
+  {
+    question: 'The nickname for the island of Ni\'ihau is ____', 
+    a: 'The Gathering Place',
+    b: 'The Garden Isle',
+    c: 'The Pineapple Isle',
+    d: 'The Forbidden Isle',
+    answer: 'd'
+  },
+  {
+    question: 'What is the official Hawaiian state mammal?', 
+    a: 'Hawaiian monk seal',
+    b: 'Northern Elephant Seal',
+    c: 'Indian Mongoose',
+    d: 'Loggerhead Turtle',
+    answer: 'a'
+  },
+  {
+    question: 'What is the warmest temperature ever recorded in Hawaii?', 
+    a: '97 F',
+    b: '99 F',
+    c: '102 F',
+    d: '100 F',
+    answer: 'd'
+  },
+  {
+    question: 'The only royal palace in the United States is the ____ palace in Honolulu.', 
+    a: 'Kahuna',
+    b: 'Mo\'i',
+    c: 'Keko',
+    d: 'Iolani',
+    answer: 'd'
+  },
+  {
+    question: 'The ancient Hawaiian code of conduct is called ____', 
+    a: 'aloha',
+    b: 'kapu',
+    c: 'muana',
+    d: 'lilo',
+    answer: 'b'
+  },
+  {
+    question: 'If a person broke kapu, the punishment included ____', 
+    a: 'punished by the powerful gods',
+    b: 'being hunted by \'ilamuku and drowned',
+    c: 'flee to pu\'uhonua (place of forgiveness) for kahuna rituals',
+    d: 'all of the above',
+    answer: 'd'
+  },
+  {
+    question: 'People in Hawaii consume ____ cans of span yearly', 
+    a: '6 million',
+    b: '600 thousand',
+    c: '60 thousand',
+    d: '6 thousand',
+    answer: 'a'
+  },
+  {
+    question: 'Hand sign, commonly known as "hang loose"', 
+    a: 'katana',
+    b: 'shaka',
+    c: 'moraka',
+    d: 'waimea',
+    answer: 'b'
+  },
+];
