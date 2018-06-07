05d671c1a9f1a576174f907d3efff7c6450a3788
Coloe, William <william.coloe@accenture.com>
2018-06-06 22:27:07 -0500

    Configure Who Am I series

--- app.js
+++ app.js
@@ -27,9 +27,9 @@ var prodRealURL = 'https://hooks.slack.com/services/T02A4DYBJ/B7WHHJC05/2lPlwdnh
 
 
 
-var webhookURL = prodSecretURL;
+var webhookURL = prodRealURL;
 
-var questions = require('./questions/question-set-01');
+var questions = require('./questions/question-set-who-am-i-1');
 
 
 
