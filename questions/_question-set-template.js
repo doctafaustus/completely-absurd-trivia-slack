d44803d78ff53e7be131ea2609d988a47a324343
doctafaustus <clowd150@gmail.com>
2018-07-06 14:13:11 -0500

    Revert to old question set template

--- questions/_question-set-template.js
+++ questions/_question-set-template.js
@@ -1,83 +1,82 @@
 module.exports = [
   {
-    question: 'What state do the Belchers live in?',
-  a: 'Maine',
-  b: 'Massachusetts',
-  c: 'New Jersey',
-  d: 'Connecticut',
-  answer: 'c'
+    question: '', 
+    a: '',
+    b: '',
+    c: '',
+    d: '',
+    answer: ''
   },
   {
-    question: 'What is Tina’s middle name?',
-  a: 'Celeste',
-  b: 'Bertha',
-  c: 'Ruth',
-  d: 'Mary',
-  answer: 'c'
+    question: '', 
+    a: '',
+    b: '',
+    c: '',
+    d: '',
+    answer: ''
   },
   {
-    question: 'Which character is voiced by the only main female cast member?',
-  a: 'Gene',
-  b: 'Louise',
-  c: 'Linda',
-  d: 'Tina',
-  answer: 'b'
+    question: '', 
+    a: '',
+    b: '',
+    c: '',
+    d: '',
+    answer: ''
   },
   {
-    question: 'When Bob and Linda opened up the house as a B&B, their first guest was Javed. What was his occupation?',
-  a: 'Entomologist',
-  b: 'Optometrist',
-  c: 'Meteorologist',
-  d: 'Geologist',
-  answer: 'a'
+    question: '', 
+    a: '',
+    b: '',
+    c: '',
+    d: '',
+    answer: ''
   },
   {
-    question: 'What instrument does Gene always have with him?',
-  a: 'harmonica',
-  b: 'keyboard',
-  c: 'drumsticks',
-  d: 'maracas',
-  answer: 'b'
+    question: '', 
+    a: '',
+    b: '',
+    c: '',
+    d: '',
+    answer: ''
   },
   {
-    question: 'Where does Bob finally find the Love-Testometer in the episode "My Fuzzy Valentine"?',
-  a: 'Drink Dynasty Lounge',
-  b: 'Peking Duck Dynasty Bar',
-  c: 'The Great Bar of China',
-  d: 'Emperor\'s Bar and Lounge',
-  answer: 'a'
+    question: '', 
+    a: '',
+    b: '',
+    c: '',
+    d: '',
+    answer: ''
   },
   {
-    question: 'What is the name of the purple store next to Jimmy Pesto\'s Pizzeria?',
-  a: 'That\'s Notable Stationery',
-  b: 'Yours Truly Stationery',
-  c: 'Staples n Stuff Stationery',
-  d: 'With Love Stationery',
-  answer: 'b'
+    question: '', 
+    a: '',
+    b: '',
+    c: '',
+    d: '',
+    answer: ''
   },
   {
-    question: 'What did the kids try to buy at Fig Jam that got them banned from coming in again?',
-  a: 'Chocolate covered edamame',
-  b: 'Black garlic',
-  c: 'Artisan gummy bears',
-  d: 'Dark chocolate with bacon and red pepper flakes',
-  answer: 'd'
+    question: '', 
+    a: '',
+    b: '',
+    c: '',
+    d: '',
+    answer: ''
   },
   {
-    question: 'Regular sized Rudy is Louise’s BFF. What’s the name of the other Rudy?',
-  a: 'Abnormally Large Rudy',
-  b: 'Pocket Sized Rudy',
-  c: 'Small Rudy',
-  d: 'Medium Sized Rudy',
-  answer: 'b'
+    question: '', 
+    a: '',
+    b: '',
+    c: '',
+    d: '',
+    answer: ''
   },
   {
-    question: 'Who does Daryl dress up as for Halloween in the episode "Fort Night"?',
-  a: 'A red crayon',
-  b: 'Dwayne "The Rock" Johnson',
-  c: 'Mark Mothersbaugh',
-  d: '1980s era Michael Jackson',
-  answer: 'c'
-  }
+    question: '', 
+    a: '',
+    b: '',
+    c: '',
+    d: '',
+    answer: ''
+  },
 ];
-
