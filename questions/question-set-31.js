139b1eb8d5a5411b297b462aa6cf25a334ad9697
doctafaustus <clowd150@gmail.com>
2018-07-18 16:57:39 -0500

    Add question set Galveston

---
+++ questions/question-set-31.js
@@ -0,0 +1,82 @@
+module.exports = [
+  {
+    question: 'Who was NOT a cast member on the TV show "Whose Line Is It Anyway?"', 
+    a: 'Paul Scheer',
+    b: 'Ryan Stiles',
+    c: 'Wayne Brady',
+    d: 'Colin Mochrie',
+    answer: 'a'
+  },
+  {
+    question: '', 
+    a: '',
+    b: '',
+    c: '',
+    d: '',
+    answer: ''
+  },
+  {
+    question: '', 
+    a: '',
+    b: '',
+    c: '',
+    d: '',
+    answer: ''
+  },
+  {
+    question: '', 
+    a: '',
+    b: '',
+    c: '',
+    d: '',
+    answer: ''
+  },
+  {
+    question: '', 
+    a: '',
+    b: '',
+    c: '',
+    d: '',
+    answer: ''
+  },
+  {
+    question: '', 
+    a: '',
+    b: '',
+    c: '',
+    d: '',
+    answer: ''
+  },
+  {
+    question: '', 
+    a: '',
+    b: '',
+    c: '',
+    d: '',
+    answer: ''
+  },
+  {
+    question: '', 
+    a: '',
+    b: '',
+    c: '',
+    d: '',
+    answer: ''
+  },
+  {
+    question: '', 
+    a: '',
+    b: '',
+    c: '',
+    d: '',
+    answer: ''
+  },
+  {
+    question: '', 
+    a: '',
+    b: '',
+    c: '',
+    d: '',
+    answer: ''
+  },
+];
