0d55bd674907df5321dd1a1c8ef0f2fda25b54bb
doctafaustus <clowd150@gmail.com>
2018-07-20 10:38:51 -0500

    Add nicknames games

---
+++ questions/question-set-nickname-1.js
@@ -0,0 +1,82 @@
+module.exports = [
+  {
+    question: 'Adam has NEVER gone by which of the following names?', 
+    a: 'Strawberry Rain',
+    b: 'Dr. Helpin',
+    c: 'Sparkles',
+    d: 'Billy',
+    answer: 'a'
+  },
+  {
+    question: 'Adrienne was known as all of these except:', 
+    a: 'Joey',
+    b: 'Slowwie',
+    c: 'CanIndian',
+    d: 'Rambo',
+    answer: 'd'
+  },
+  {
+    question: 'Autumn has also been known as:', 
+    a: 'Apple Pie',
+    b: 'Lil Tran',
+    c: 'Apes',
+    d: 'Penny',
+    answer: 'c'
+  },
+  {
+    question: 'This person might respond to the nickname "The Mistake"', 
+    a: 'Libby',
+    b: 'JoCo',
+    c: 'Couv',
+    d: 'Mercan',
+    answer: 'a'
+  },
+  {
+    question: 'ChelsB’s (other) nickname is:', 
+    a: 'Chelsyonnce',
+    b: 'Cheesy',
+    c: 'Chelsney',
+    d: 'Cheech',
+    answer: 'b'
+  },
+  {
+    question: 'Kevin was once known as:', 
+    a: 'The Big League Chew',
+    b: 'Doc Oc(topus)',
+    c: 'Magic Mustache',
+    d: 'None of the Above',
+    answer: 'd'
+  },
+  {
+    question: 'Who goes by the title "Rainy Day"?', 
+    a: 'Darice',
+    b: 'Glynn',
+    c: 'Jessica',
+    d: 'Marion',
+    answer: 'd'
+  },
+  {
+    question: 'Which individual goes by the name "Eurotrash"?', 
+    a: 'Mark P',
+    b: 'Billy',
+    c: 'Marcus',
+    d: 'Tyler N',
+    answer: 'a'
+  },
+  {
+    question: 'What is Sean\'s nickname?', 
+    a: 'Flea',
+    b: 'Black Ice',
+    c: 'The Situation',
+    d: 'FatBoy Slim',
+    answer: 'b'
+  },
+  {
+    question: 'What is Joe\'s nickname?', 
+    a: 'Viewtiful Joe',
+    b: 'Mighty Joe Old',
+    c: 'Bum',
+    d: 'None of the Above',
+    answer: 'd'
+  },
+];
---
+++ questions/question-set-nicknames-2.js
@@ -0,0 +1,82 @@
+module.exports = [
+  {
+    question: 'Who is also known as "Bubba"?', 
+    a: 'Taylor',
+    b: 'Tom',
+    c: 'Troy',
+    d: 'JoCo',
+    answer: 'd'
+  },
+  {
+    question: 'Which one of these nicknames is NOT owned by Billy?', 
+    a: 'Wild Bill',
+    b: 'The Human Tampon',
+    c: 'Mr. Smooth',
+    d: 'Galileo',
+    answer: 'd'
+  },
+  {
+    question: 'What is Haley’s nickname?', 
+    a: 'Paley',
+    b: 'Scarlett Fever',
+    c: 'Irish Heat',
+    d: 'None of the Above',
+    answer: 'd'
+  },
+  {
+    question: 'Who goes by the nickname "Mean Mug"?', 
+    a: 'Molly',
+    b: 'Dante',
+    c: 'Tom',
+    d: 'None of the Above',
+    answer: 'a'
+  },
+  {
+    question: 'Brady goes by which of the following WORK nicknames?', 
+    a: 'Bizzle',
+    b: 'B-Rad',
+    c: 'Bray-quan',
+    d: 'None of the Above',
+    answer: 'c'
+  },
+  {
+    question: 'Kellee was also known as which names?', 
+    a: 'Kimberlee',
+    b: 'Keezy',
+    c: 'Baby',
+    d: 'None of the Above',
+    answer: 'a'
+  },
+  {
+    question: 'Which of these is NOT a Turkish nickname for Mercan?', 
+    a: 'Spaghetti',
+    b: 'Naber',
+    c: 'Patates',
+    d: 'None of the Above',
+    answer: 'd'
+  },
+  {
+    question: 'What is Troy\'s nickname?', 
+    a: 'Shooter McAcosta',
+    b: 'Jackhammer Junglecat',
+    c: 'The Big Pickle',
+    d: 'Chanco',
+    answer: 'b'
+  },
+  {
+    question: 'Which nickname does Ryan Abelman NOT go by?', 
+    a: 'Rez',
+    b: 'Buckeye',
+    c: 'Paminto',
+    d: 'None of the Above',
+    answer: 'd'
+  },
+  {
+    question: 'Who has the childhood nickname Motts?', 
+    a: 'BDF',
+    b: 'Brandon',
+    c: 'Molly',
+    d: 'Devon',
+    answer: 'c'
+  },
+];
