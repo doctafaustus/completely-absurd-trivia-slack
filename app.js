72a978f440d31b140c384582102ebbecd937fbb3
doctafaustus <clowd150@gmail.com>
2018-07-20 12:18:50 -0500

    Configure question set nicknames 2

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ var prodRealURL = 'https://hooks.slack.com/services/T02A4DYBJ/B7WHHJC05/2lPlwdnh
 
 var webhookURL = prodRealURL;
 
-var questions = require('./questions/question-set-nicknames-1.js');
+var questions = require('./questions/question-set-nicknames-2.js');
 
 
 
