600a12ac2af291f211c4599cdce122a22bc09f45
doctafaustus <clowd150@gmail.com>
2018-08-25 14:45:12 -0500

    Add question set 31

---
+++ questions/question-set-31.js
@@ -0,0 +1,82 @@
+module.exports = [
+  {
+    question: 'What is Weird Al Yankovic\'s first name?', 
+    a: 'Weird',
+    b: 'Jebediah',
+    c: 'Matthew',
+    d: 'Alfred',
+    answer: 'd'
+  },
+  {
+    question: 'What condition does "Denorex" treat?', 
+    a: 'Nail Fungus',
+    b: 'Dandruff',
+    c: 'Excessive Flatulence',
+    d: 'Erectile Dysfunction',
+    answer: 'b'
+  },
+  {
+    question: 'A female fox is called a ___', 
+    a: 'Fienna',
+    b: 'Vixen',
+    c: 'Fennec',
+    d: 'Reynard',
+    answer: 'b'
+  },
+  {
+    question: 'What shoe company donates a pair of shoes to an impoverished child with every purchase?', 
+    a: 'Naot',
+    b: 'Keds',
+    c: 'TOMS',
+    d: 'Vans',
+    answer: 'c'
+  },
+  {
+    question: 'Who was famous for the phrase "The unexamined life is not worth living"?', 
+    a: 'Plato',
+    b: 'Socrates',
+    c: 'William Shakespeare',
+    d: 'John Donne',
+    answer: 'b'
+  },
+  {
+    question: 'Which of the following is NOT a La Croix flavor?', 
+    a: 'Kiwi Sandia',
+    b: 'Mure Pepino',
+    c: 'Naranja Platano',
+    d: 'Pomme Baya',
+    answer: 'c'
+  },
+  {
+    question: 'What album did Enimen\'s song "The Real Slim Shady" appear on?', 
+    a: 'The Slim Shady LP ',
+    b: 'The Marshall Mathers LP',
+    c: 'The Eminem Show',
+    d: 'Encore',
+    answer: 'b'
+  },
+  {
+    question: 'Who is the official mascot for Pringles chips?', 
+    a: 'Mr. Pringle',
+    b: 'Julius Pringles',
+    c: 'Chester',
+    d: 'Pritchard Pringle',
+    answer: 'b'
+  },
+  {
+    question: 'Which of the following was named "Sexiest Man Alive" by People Magazine?', 
+    a: 'Christian Bale',
+    b: 'Mel Gibson',
+    c: 'Robert Downey Jr.',
+    d: 'Will Smith',
+    answer: 'b'
+  },
+  {
+    question: 'What was the name of Aladdin\'s pet monkey?', 
+    a: 'Bubu',
+    b: 'Iago',
+    c: 'Abu',
+    d: 'Abdul',
+    answer: 'c'
+  },
+];
