bbc94102ad35e32c65f4d1cc15c263478df0aa77
doctafaustus <clowd150@gmail.com>
2018-09-03 15:25:01 -0500

    Add and configure question set 32

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ var prodRealURL = 'https://hooks.slack.com/services/T02A4DYBJ/B7WHHJC05/2lPlwdnh
 
 var webhookURL = prodRealURL;
 
-var questions = require('./questions/question-set-31.js');
+var questions = require('./questions/question-set-32.js');
 
 
 
---
+++ questions/question-set-32.js
@@ -0,0 +1,82 @@
+module.exports = [
+  {
+    question: 'Who created "Beavis and Butt-Head" and "King of the Hill"?', 
+    a: 'Mike Judge',
+    b: 'Matt Groening',
+    c: 'Seth MacFarlane',
+    d: 'Stephen Hillenburg',
+    answer: 'a'
+  },
+  {
+    question: '"Zorro" is Spanish for what?', 
+    a: 'Tiger',
+    b: 'Fox',
+    c: 'Phantom',
+    d: 'Hero',
+    answer: 'b'
+  },
+  {
+    question: 'Who is the main character in the video game series "Metroid"?', 
+    a: 'Simon Belmont',
+    b: 'Mega Man',
+    c: 'Captain Falcon',
+    d: 'Samas Aran',
+    answer: 'd'
+  },
+  {
+    question: 'Where is Mount Vesuvius located?', 
+    a: 'Egypt',
+    b: 'Greece',
+    c: 'Italy',
+    d: 'Nepal',
+    answer: 'c'
+  },
+  {
+    question: 'What company\'s products were originally developed for laboratory use?', 
+    a: 'Coleman',
+    b: 'Nalgene',
+    c: 'YETI',
+    d: 'Igloo',
+    answer: 'b'
+  },
+  {
+    question: 'Who was the widely criticized head of FEMA during Hurricane Katrina?', 
+    a: 'Michael Brown',
+    b: 'Brock Long',
+    c: 'Craig Fugate',
+    d: 'John Macy',
+    answer: 'a'
+  },
+  {
+    question: 'Which city is known as the "Biggest Little City in the World"?', 
+    a: 'Reno',
+    b: 'Las Vegas',
+    c: 'Tuscon',
+    d: 'Salt Lake City',
+    answer: 'a'
+  },
+  {
+    question: 'Ewoks are native to which location?', 
+    a: 'Endor',
+    b: 'Tatooine',
+    c: 'Alderaan',
+    d: 'Zebes',
+    answer: 'a'
+  },
+  {
+    question: 'Which actor is famous for playing the role of Count Dracula?', 
+    a: 'Vincent Price',
+    b: 'Christopher Lee',
+    c: 'Lon Chaney',
+    d: 'Boris Karloff',
+    answer: 'b'
+  },
+  {
+    question: 'Which political commentator had once co-hosted CNN\'s "Crossfire" and now headlines their own show on Fox News?', 
+    a: 'Bill O\'Reilly',
+    b: 'Glenn Beck',
+    c: 'Megyn Kelly',
+    d: 'Tucker Carlson',
+    answer: 'd'
+  },
+];
