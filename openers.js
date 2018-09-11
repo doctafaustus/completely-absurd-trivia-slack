69adb5792122a893d644c4489ed507753889a6b2
doctafaustus <clowd150@gmail.com>
2018-09-10 23:17:15 -0500

    Add openers

---
+++ openers.js
@@ -0,0 +1,17 @@
+var openers = [
+  'is about to go Loconut',
+  'is feeling fine and dandy like sour candy',
+  'just smacked everyone\'s butt with a yard o\' beef',
+  'is definitely not the Tangerine Bandit',
+  'might secretly enjoy Lifehouse',
+  'is fat-cattin\' all day, every day',
+  'has lunchspiration problems',
+  'can\'t spin a leek for beans',
+  'doesn\'t have anything to hide, but still wears shorts in the shower',
+  'both smelt it and delt it',
+  'just developed irritable bowel syndrome',
+  'was spotted drinking appletinis at the bowling alley bar',
+  'ate too much kringle today',
+  '',
+  // Ask reddit
+];
