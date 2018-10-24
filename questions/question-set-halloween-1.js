2a85bffa6951bcf9d00362e86aa48256d72a4289
doctafaustus <clowd150@gmail.com>
2018-10-23 21:16:19 -0500

    Add question set Halloween 1

---
+++ questions/question-set-halloween-1.js
@@ -0,0 +1,82 @@
+module.exports = [
+  {
+    question: 'Which of the following is NOT an effective way to kill a vampire?', 
+    a: 'Suffocation',
+    b: 'Holy Water',
+    c: 'Sunlight',
+    d: 'Silver Bullet',
+    answer: 'a'
+  },
+  {
+    question: 'Which actor was famous for playing the part of Doctor Van Helsing?', 
+    a: 'Christopher Lee',
+    b: 'Bela Lugosi',
+    c: 'Peter Cushing',
+    d: 'Ian McKellen',
+    answer: 'c'
+  },
+  {
+    question: 'What was the original name for candy corn used in the late 1800s?', 
+    a: 'Chicken Feed',
+    b: 'Wax Fingers',
+    c: 'Little Jellies',
+    d: 'Wax Plugs',
+    answer: 'a'
+  },
+  {
+    question: 'Which of following is NOT one of the seven "Princes of Hell" nor associated with any of the seven deadly sins?', 
+    a: 'Lucifier',
+    b: 'Leviathan',
+    c: 'Beelzebub',
+    d: 'Kali',
+    answer: 'd'
+  },
+  {
+    question: 'Who wrote the story "The Legend of Sleepy Hollow?"', 
+    a: 'Washington Irving',
+    b: 'HG Wells',
+    c: 'Bram Stoker',
+    d: 'Edgar Allen Poe',
+    answer: 'a'
+  },
+  {
+    question: 'What is the most well known treatise on witchcraft?', 
+    a: 'The Confession of Ellemar',
+    b: 'From the Far Reaches of Hell',
+    c: 'Malleus Maleficarum',
+    d: 'The Satanic Bible',
+    answer: 'c'
+  },
+  {
+    question: 'Gargoyles were used to frighten intruders from the buildings they guard, but they also served as what?', 
+    a: 'Rainspouts',
+    b: 'Chimneys',
+    c: 'Lanterns',
+    d: 'Lookouts',
+    answer: 'a'
+  },
+  {
+    question: 'According to a 1902 supernatural short story by WW Jacobs, which item can grant 3 wishes but at a cost for interfering with fate?', 
+    a: 'Dandelion',
+    b: 'Monkey Paw',
+    c: 'Dragon Ball',
+    d: 'Coin',
+    answer: 'b'
+  },
+  {
+    question: 'The jack-o-lantern was named after which Jack?', 
+    a: 'Jack Skellington',
+    b: 'Stingy Jack',
+    c: 'Jack the Ripper',
+    d: 'Jack Black',
+    answer: 'b'
+  },
+  {
+    question: 'What is a man who practices witchcraft called?', 
+    a: 'Wizard',
+    b: 'Warlock',
+    c: 'Witch Doctor',
+    d: 'Male Witch',
+    answer: 'b'
+  },
+];
