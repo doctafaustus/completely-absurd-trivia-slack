6f82608decb5776b218dd4ef9c3e07d1b0e40450
doctafaustus <clowd150@gmail.com>
2018-12-14 14:29:02 -0600

    Add and configure question set james xmas

---
+++ questions/question-set-james-xmas.js
@@ -0,0 +1,82 @@
+module.exports = [
+  {
+    question: 'What unrefined element is the fictional element Vibranium, from Black Panther, based on?', 
+    a: 'Coltan',
+    b: 'Chromium',
+    c: 'Mercury',
+    d: 'Cadmium',
+    answer: 'a'
+  },
+  {
+    question: 'What type of wood is most used to make pencils?', 
+    a: 'Pine',
+    b: 'Balsa',
+    c: 'Cedar',
+    d: 'Beech',
+    answer: 'c'
+  },
+  {
+    question: 'What are the concrete walls separating traffic on highways called?', 
+    a: 'New York dividers',
+    b: 'Jersey barriers',
+    c: 'Chicago splitters',
+    d: 'Border walls',
+    answer: 'b'
+  },
+  {
+    question: 'Who invented the JavaScript programming language?', 
+    a: 'Troy Acosta',
+    b: 'Bill Gates',
+    c: 'Brendan Eich',
+    d: 'Steve Ballmer',
+    answer: 'c'
+  },
+  {
+    question: 'Who owns the 7UP brand in the US?', 
+    a: 'Pepsi Co',
+    b: 'CocaCola',
+    c: 'Kuerig Dr. Pepper',
+    d: 'Nestle',
+    answer: 'c'
+  },
+  {
+    question: 'What was Nic Cage\'s first movie?', 
+    a: 'Raising Arizona',
+    b: 'Valley Girl',
+    c: 'Moonstruck',
+    d: 'Fast Times at Ridgemont High',
+    answer: 'd'
+  },
+  {
+    question: 'Who wrote Jingle Bells?', 
+    a: 'Jim from the Office',
+    b: 'James Lord Pierpont',
+    c: 'James Summers',
+    d: 'James Pachelbel',
+    answer: 'b'
+  },
+  {
+    question: 'What # is the new conference room in HQ2(Pandora)?', 
+    a: 'CR8',
+    b: 'CR9',
+    c: 'CR10',
+    d: 'John C Reilly',
+    answer: 'a'
+  },
+  {
+    question: 'How many nights does Hanukkah last?', 
+    a: '7',
+    b: '8',
+    c: '2',
+    d: '12',
+    answer: 'b'
+  },
+  {
+    question: 'What does auld lang syne mean?', 
+    a: 'The good old days',
+    b: 'Happy days',
+    c: 'Old language Sign',
+    d: 'Times long past',
+    answer: 'd'
+  },
+];
