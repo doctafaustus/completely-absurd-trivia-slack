ad55b08dcb8e9b178410310353be5e447676c647
doctafaustus <clowd150@gmail.com>
2019-01-14 16:15:29 -0600

    Add and configure qs 35

---
+++ questions/question-set-35.js
@@ -0,0 +1,82 @@
+module.exports = [
+  {
+    question: 'What area of the body is the duodenum located?', 
+    a: 'Head',
+    b: 'Stomach',
+    c: 'Pelvis',
+    d: 'Chest',
+    answer: 'b'
+  },
+  {
+    question: 'Which brand of paper towels used the slogan "The Quicker Picker Upper!"?', 
+    a: 'Brawny',
+    b: 'Sparkle',
+    c: 'Bounty',
+    d: 'Scott',
+    answer: 'c'
+  },
+  {
+    question: 'Who composed the 1877 romantic ballet "Swan Lake"?', 
+    a: 'Tchaikovsky',
+    b: 'Chopin',
+    c: 'Wagner',
+    d: 'Beethoven',
+    answer: 'a'
+  },
+  {
+    question: 'What audience measurement system is used for television programming?', 
+    a: 'ESRB Ratings',
+    b: 'Alexa Rankings',
+    c: 'Pew Research Method',
+    d: 'Nielsen Ratings',
+    answer: 'd'
+  },
+  {
+    question: 'The Internet protocol suite is also known as ____.', 
+    a: 'TCP/IP',
+    b: 'UDP',
+    c: 'IPv6',
+    d: 'DNS',
+    answer: 'a'
+  },
+  {
+    question: 'Which modern states did New Netherland encompass in the 17th century?', 
+    a: 'NY and NJ',
+    b: 'VA and WV',
+    c: 'NC and SC',
+    d: 'NH and VT',
+    answer: 'a'
+  },
+  {
+    question: 'Which of the following is a vestigial-structure?', 
+    a: 'Paper Clip',
+    b: 'Human Appendix',
+    c: 'Golgi-Body',
+    d: 'Mucus Membrane',
+    answer: 'b'
+  },
+  {
+    question: 'What plane figure has five straight sides and five angles', 
+    a: 'Trapezoid',
+    b: 'Decagon',
+    c: 'Pentagon',
+    d: 'Heptagon',
+    answer: 'c'
+  },
+  {
+    question: 'Who was the main antagonist in The Hunchback of Notre-Dame?', 
+    a: 'Amos Slade',
+    b: 'Quasimodo',
+    c: 'Gaston',
+    d: 'Claude Frollo',
+    answer: 'd'
+  },
+  {
+    question: 'Who was the winner of the first season the reality show "Survivor"?', 
+    a: 'Richard Hatch',
+    b: 'Regis Philbin',
+    c: 'Sue Hawk',
+    d: 'John Cochran',
+    answer: 'a'
+  },
+];
