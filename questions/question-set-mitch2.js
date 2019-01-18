2628f1c6857a319d1b6f7628290c25be560f2994
doctafaustus <clowd150@gmail.com>
2019-01-17 22:35:09 -0600

    Add qs Mitch

---
+++ questions/question-set-mitch2.js
@@ -0,0 +1,82 @@
+module.exports = [
+  {
+    question: 'Where does Abbi work (seasons 1 – 3) in Broad City?',
+    a: 'Jumbo Juice',
+    b: 'Soulstice',
+    c: 'Dave & Busters',
+    d: 'Booklyn',
+    answer: 'b'
+  },
+  {
+    question: 'Which house does Tyrion belong to in Game of Thrones?',
+    a: 'Lannister',
+    b: 'Stark',
+    c: 'Targaryen',
+    d: 'Bolton',
+    answer: 'a'
+  },
+  {
+    question: 'Which TV host was NOT formerly a correspondent for the Daily Show?',
+    a: 'John Oliver',
+    b: 'Samantha Bee',
+    c: 'Stephen Colbert',
+    d: 'Seth Meyers',
+    answer: 'd'
+  },
+  {
+    question: "What is the name of the fictional corporation that runs 'Westworld'?",
+    a: 'Forde',
+    b: 'Centurii',
+    c: 'Delos',
+    d: 'Blackhat',
+    answer: 'c'
+  },
+  {
+    question: 'What theme park is down the street from Bobs Burgers?',
+    a: 'Wonder Wharf',
+    b: 'Dolphin Dock',
+    c: 'Pookah Park',
+    d: 'Water Walk',
+    answer: 'd'
+  },
+  {
+    question: 'In season 1 of Atlanta, which item is successfully exchanged for a dog?',
+    a: 'painting',
+    b: 'samurai sword',
+    c: '2 pirate coins',
+    d: 'storage locker key',
+    answer: 'b'
+  },
+  {
+    question: "What is The Marvelous Ms. Maisel's first name?",
+    a: 'Anne',
+    b: 'Midge',
+    c: 'Yael',
+    d: 'Rachel',
+    answer: 'b'
+  },
+  {
+    question: 'Which Marvel TV show was NOT cancelled this year?',
+    a: 'Daredevil',
+    b: 'Luke Cage',
+    c: 'Iron Fist',
+    d: 'All were cancelled',
+    answer: 'd'
+  },
+  {
+    question: "In the show Homecoming, what is Julia Roberts' occupation?",
+    a: 'Neurosurgeon',
+    b: 'Lobbyist',
+    c: 'Caseworker',
+    d: 'Hypnotist',
+    answer: 'c'
+  },
+  {
+    question: "Which of these is NOT a curse word substitute on The Good Place?",
+    a: 'Fork',
+    b: 'Shirtballs',
+    c: 'Here',
+    d: 'Pez',
+    answer: 'd'
+  },
+];
