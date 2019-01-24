0e53f3e688738b5065119708314040138cc341fe
doctafaustus <clowd150@gmail.com>
2019-01-24 01:20:30 -0600

    Add qs 36

---
+++ questions/question-set-36.js
@@ -0,0 +1,82 @@
+module.exports = [
+  {
+    question: 'Which branch of the U.S. Armed Forces operates under the Department of Homeland Security during peacetime?', 
+    a: 'Coast Guard',
+    b: 'National Guard',
+    c: 'Navy',
+    d: 'Air Force',
+    answer: 'a'
+  },
+  {
+    question: 'What is a female pig called?', 
+    a: 'Boar',
+    b: 'Sow',
+    c: 'Piglet',
+    d: 'Swine',
+    answer: 'b'
+  },
+  {
+    question: 'What ailment does the medicine Nexium treat?', 
+    a: 'Eczema',
+    b: 'Flatulence',
+    c: 'Impotence',
+    d: 'Heartburn',
+    answer: 'd'
+  },
+  {
+    question: '"Up-Up-Down-Down-Left-Right-Left-Right B, A, Start" is also known as what?', 
+    a: 'The Da Vinci Code',
+    b: 'The Konami Code',
+    c: 'The Caesar Shift',
+    d: 'RSA Encryption',
+    answer: 'b'
+  },
+  {
+    question: 'What is the National Mammal of the United States?', 
+    a: 'Bald Eagle',
+    b: 'Bison',
+    c: 'Grizzly Bear',
+    d: 'Dog',
+    answer: 'b'
+  },
+  {
+    question: 'Which band wrote the popular stadium anthem-like song "The Final Countdown"?', 
+    a: 'Van Halen',
+    b: 'Bon Jovi',
+    c: 'Scorpions',
+    d: 'Europe',
+    answer: 'a'
+  },
+  {
+    question: 'Who was first American astronaut to travel into space?', 
+    a: 'Buzz Aldrin',
+    b: 'Neil Armstrong',
+    c: 'Alan Shepard',
+    d: 'Lance Armstrong',
+    answer: 'c'
+  },
+  {
+    question: 'Which of the following is NOT a skateboarding trick?', 
+    a: 'Snausage',
+    b: 'Nollie',
+    c: 'Ollie',
+    d: 'Grind',
+    answer: 'a'
+  },
+  {
+    question: 'What sandwich is composed of corned beef, Swiss cheese, sauerkraut, and Russian dressing, grilled between slices of rye bread?', 
+    a: 'Porchetta',
+    b: 'Reuben',
+    c: 'Monte Cristo',
+    d: 'Cuban',
+    answer: 'b'
+  },
+  {
+    question: 'Yahoo, LinkedIn, and NetApp are all headquartered where?', 
+    a: 'San Francisco, CA',
+    b: 'Sunnyvale, CA',
+    c: 'Palo Alto, CA',
+    d: 'Redmond, WA',
+    answer: 'b'
+  },
+];
