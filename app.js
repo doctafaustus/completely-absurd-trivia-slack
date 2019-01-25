8791040952bdca92dc2e3d9ec00ead38d7d317f7
doctafaustus <clowd150@gmail.com>
2019-01-25 13:19:43 -0600

    Config qs 37

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ var prodRealURL = 'https://hooks.slack.com/services/T02A4DYBJ/B7WHHJC05/2lPlwdnh
 
 var webhookURL = prodRealURL;
 
-var questions = require('./questions/question-set-36');
+var questions = require('./questions/question-set-37');
 
 
 
