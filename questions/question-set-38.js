cec11fcc5232a4b3e3b47f1101333a455888a017
doctafaustus <clowd150@gmail.com>
2019-06-05 21:31:22 -0500

    Begin qs 38

---
+++ questions/question-set-38.js
@@ -0,0 +1,82 @@
+module.exports = [
+  {
+    question: 'Who is the spokesperson of Icy Hot?', 
+    a: 'Ben Stein',
+    b: 'Common',
+    c: 'Shaquille O\'Neal',
+    d: 'Stephen Colbert',
+    answer: 'c'
+  },
+  {
+    question: 'Which of the following is not an Atlassian product?', 
+    a: 'Trello',
+    b: 'Basecamp',
+    c: 'Confluence',
+    d: 'Jira',
+    answer: 'b'
+  },
+  {
+    question: 'Vince Offer was the pitchman for what product?', 
+    a: 'ShamWow',
+    b: 'Oxiclean',
+    c: 'Snuggie',
+    d: 'Magic Bullet',
+    answer: 'a'
+  },
+  {
+    question: 'Which of the following HTML tags would you use to create a list with each item numbered?', 
+    a: '<ol>',
+    b: '<ul>',
+    c: '<nl>',
+    d: '<dl>',
+    answer: 'a'
+  },
+  {
+    question: 'What company makes Oeros?', 
+    a: 'General Mills',
+    b: 'Nabisco',
+    c: 'Nestle',
+    d: 'Pillsbury',
+    answer: 'b'
+  },
+  {
+    question: '', 
+    a: '',
+    b: '',
+    c: '',
+    d: '',
+    answer: ''
+  },
+  {
+    question: '', 
+    a: '',
+    b: '',
+    c: '',
+    d: '',
+    answer: ''
+  },
+  {
+    question: '', 
+    a: '',
+    b: '',
+    c: '',
+    d: '',
+    answer: ''
+  },
+  {
+    question: '', 
+    a: '',
+    b: '',
+    c: '',
+    d: '',
+    answer: ''
+  },
+  {
+    question: '', 
+    a: '',
+    b: '',
+    c: '',
+    d: '',
+    answer: ''
+  },
+];
