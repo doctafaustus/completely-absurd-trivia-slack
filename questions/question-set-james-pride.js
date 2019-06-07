58767676bd406a5dfa3904390efb188259ffb336
doctafaustus <clowd150@gmail.com>
2019-06-07 10:23:33 -0500

    Add qs James pride

---
+++ questions/question-set-james-pride.js
@@ -0,0 +1,82 @@
+module.exports = [
+  {
+    question: 'When was the first gay and lesbian march on Washington?', 
+    a: '1990',
+    b: '1985',
+    c: '1981',
+    d: '1965',
+    answer: 'd'
+  },
+  {
+    question: 'In what year did the APA remove homosexuality from its list of mental disorders?', 
+    a: '1971',
+    b: '1973',
+    c: '1977',
+    d: '1980',
+    answer: 'b'
+  },
+  {
+    question: 'How many stripes does the Rainbow Flag currenlty have?', 
+    a: '8',
+    b: '7',
+    c: '6',
+    d: '5',
+    answer: 'c'
+  },
+  {
+    question: 'Which New England state was the first to make gay marriage legal?', 
+    a: 'Massachussets',
+    b: 'Connecticut',
+    c: 'New Hampshire',
+    d: 'Vermont',
+    answer: 'd'
+  },
+  {
+    question: 'What gay African-American author\'s works include "The Fire Next Time" & "Sonny\'s Blues"?' , 
+    a: 'James Baldwin',
+    b: 'Barbara Smith',
+    c: 'Essex Hemphill',
+    d: 'Sapphire',
+    answer: 'a'
+  },
+  {
+    question: 'What US state was the first to decriminalize homosexuality?', 
+    a: 'California',
+    b: 'Illinois',
+    c: 'New York',
+    d: 'Ohio',
+    answer: 'b'
+  },
+  {
+    question: 'What country was the first in the world to legalize gay marriage?', 
+    a: 'France',
+    b: 'Denmark',
+    c: 'The Netherlands',
+    d: 'Spain',
+    answer: 'c'
+  },
+  {
+    question: 'What US state was the first to outlaw discrimination based on sexual orientation?', 
+    a: 'New York',
+    b: 'Colorado',
+    c: 'Wisconsin',
+    d: 'Minnesota',
+    answer: 'c'
+  },
+  {
+    question: 'What famous gay rights activist was murdered by the San Francisco City Supervisor, Dan White?', 
+    a: 'Matthew Shephard',
+    b: 'Tennessee Williams',
+    c: 'Dick Button',
+    d: 'Harvey Milk',
+    answer: 'd'
+  },
+  {
+    question: 'When was the first gay pride parade held in the US?', 
+    a: '1968',
+    b: '1969',
+    c: '1970',
+    d: '1971',
+    answer: 'c'
+  },
+];
