ff27f8639e2eeb087c66f4b6bc147c5e4d1b17c2
doctafaustus <clowd150@gmail.com>
2019-08-06 16:14:04 -0500

    Add qs 40

---
+++ questions/question-set-40.js
@@ -0,0 +1,82 @@
+module.exports = [
+  {
+    question: 'What is the most widely eaten fish in the world?', 
+    a: 'Herring',
+    b: 'Tuna',
+    c: 'Salmon',
+    d: 'Bass',
+    answer: 'a'
+  },
+  {
+    question: 'Which Teenage Mutant Ninja Turtle wears red as his signature color?', 
+    a: 'Leonardo',
+    b: 'Michelangelo',
+    c: 'Donatello',
+    d: 'Raphael',
+    answer: 'd'
+  },
+  {
+    question: 'Cognac is a type of _____.', 
+    a: 'Brandy',
+    b: 'Whiskey',
+    c: 'Vodka',
+    d: 'Tequila',
+    answer: 'a'
+  },
+  {
+    question: 'In cooking, what is "ghee"?', 
+    a: 'Butter',
+    b: 'Rice',
+    c: 'A Vegetable',
+    d: 'A Spice',
+    answer: 'a'
+  },
+  {
+    question: 'Who is the mother of Sean Preston Federline?', 
+    a: 'Jennifer Lopez',
+    b: 'Christina Aguilera',
+    c: 'Naomi Lowde-Priestley',
+    d: 'Britney Spears',
+    answer: 'd'
+  },
+  {
+    question: 'Which of the following animals is an arthropod?', 
+    a: 'Lizard',
+    b: 'Squid',
+    c: 'Spider',
+    d: 'Fish',
+    answer: 'c'
+  },
+  {
+    question: 'Which theme park was initially developed as a marketing vehicle for a brewing company?', 
+    a: 'Gilroy Gardens',
+    b: 'Knott\'s Berry Farm',
+    c: 'Six Flags',
+    d: 'Busch Gardens',
+    answer: 'd'
+  },
+  {
+    question: 'General Motors uses which stock symbol?', 
+    a: 'GENMO',
+    b: 'GM',
+    c: 'GEMO',
+    d: 'GMOTO',
+    answer: 'b'
+  },
+  {
+    question: 'What is a style of Japanese comic books and graphic novels, aimed at both adults and children?', 
+    a: 'Naruto',
+    b: 'Batsu',
+    c: 'Manga',
+    d: 'Getsuku',
+    answer: 'c'
+  },
+  {
+    question: 'What is the Canadia one-dollar coin called?', 
+    a: 'Toonie',
+    b: 'Loonie',
+    c: 'Boonie',
+    d: 'Doonie',
+    answer: 'b'
+  },
+];
