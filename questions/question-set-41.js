1d915ff96529d3ee19b6ad8f6871d4d16e7ae414
doctafaustus <clowd150@gmail.com>
2019-08-08 12:51:13 -0500

    Upload qs 40 & 41

---
+++ questions/question-set-41.js
@@ -0,0 +1,82 @@
+module.exports = [
+  {
+    question: 'In March 2019, what aircraft was grounded due to an erroneous software system?', 
+    a: 'Airbus A340',
+    b: 'Airbus A380-800',
+    c: 'Boeing 737 MAX',
+    d: 'Boeing 777',
+    answer: 'c'
+  },
+  {
+    question: 'How many cards are in a standard deck including the joker cards?', 
+    a: '54',
+    b: '52',
+    c: '50',
+    d: '56',
+    answer: 'a'
+  },
+  {
+    question: 'What is a baby goat called?', 
+    a: 'Fawn',
+    b: 'Veal',
+    c: 'Calf',
+    d: 'Kid',
+    answer: 'd'
+  },
+  {
+    question: 'In the US Government, who is in charge of the Executive Branch?', 
+    a: 'The President',
+    b: 'The Chief Justice',
+    c: 'Congress',
+    d: 'Senate',
+    answer: 'a'
+  },
+  {
+    question: 'What was the first music video played on MTV?', 
+    a: 'Da Ya Think I\'m Sexy',
+    b: 'Money For Nothing',
+    c: 'In the Air Tonight',
+    d: 'Video Killed the Radio Star',
+    answer: 'd'
+  },
+  {
+    question: 'Which of the following is NOT a Taco Bell Menu item?', 
+    a: 'Nacho Fries',
+    b: 'Chaluptra',
+    c: 'Crunchwrap',
+    d: 'Gordita',
+    answer: 'b'
+  },
+  {
+    question: 'Where is New Glasgow located?', 
+    a: 'Nova Scotia',
+    b: 'Brazil',
+    c: 'Ireland',
+    d: 'Holland',
+    answer: 'a'
+  },
+  {
+    question: 'Which of the following is an ingredient in brown sugar?', 
+    a: 'Corn Syrup',
+    b: 'Agave Nectar',
+    c: 'Maple Syrup',
+    d: 'Molasses',
+    answer: 'd'
+  },
+  {
+    question: 'What was the name of the Jetsons family dog?', 
+    a: 'Neptune',
+    b: 'Astro',
+    c: 'Comet',
+    d: 'Spike',
+    answer: 'b'
+  },
+  {
+    question: 'Which genre of music typically features the double bass drum?', 
+    a: 'House',
+    b: 'Heavy Metal',
+    c: 'EDM',
+    d: 'Folk',
+    answer: 'b'
+  },
+];
