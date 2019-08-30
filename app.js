46bcf73af38ded41396eed5b930139785e42127c
doctafaustus <clowd150@gmail.com>
2019-08-30 14:20:58 -0500

    Add and config qs james-hip-hop

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ var prodRealURL = process.env.prodRealURL;
 
 var webhookURL = prodRealURL;
 
-var questions = require('./questions/question-set-41.js');
+var questions = require('./questions/question-set-james-hip-hop.js');
 
 
 
---
+++ questions/question-set-james-hip-hop.js
@@ -0,0 +1,82 @@
+module.exports = [
+  {
+    question: 'What is Kendrick Lamar\'s last name?', 
+    a: 'Lamar',
+    b: 'Kendrick',
+    c: 'Duckworth',
+    d: 'Knowles',
+    answer: 'c'
+  },
+  {
+    question: 'What is O\'Shea Jackson\'s rap alias?', 
+    a: 'Ice T',
+    b: 'Ice Cube',
+    c: 'Vanilla Ice',
+    d: 'Billy the Wizard',
+    answer: 'b'
+  },
+  {
+    question: 'Which hip hop artist was born in Austin?', 
+    a: 'Missy Elliott',
+    b: 'Ciara',
+    c: 'Lil Kim',
+    d: 'Nicki Minaj',
+    answer: 'b'
+  },
+  {
+    question: 'Which of these is NOT one of 50 Cent\'s albums?', 
+    a: 'Get Rich or Die Trying',
+    b: 'Massacre',
+    c: 'Beg For Mercy',
+    d: 'Chicken and Beer',
+    answer: 'd'
+  },
+  {
+    question: 'How old is Pharrell?', 
+    a: '34',
+    b: '37',
+    c: '42',
+    d: '46',
+    answer: 'd'
+  },
+  {
+    question: 'What was Wreckx-n-Effect\'s most popular single in 1992?', 
+    a: 'Here We Come',
+    b: 'Tell Me How You Feel',
+    c: 'Rump Shaker',
+    d: 'My Cutie',
+    answer: 'c'
+  },
+  {
+    question: 'What does N.E.R.D. stand for?', 
+    a: 'No-one Ever Really Dies',
+    b: 'Never Ever Run Distances',
+    c: 'No Early Rising Dude',
+    d: 'Nancy Expresses Real Disdain',
+    answer: 'a'
+  },
+  {
+    question: 'What is the title of Tyler, The Creator\'s latest album?', 
+    a: 'EARFQUAKE',
+    b: 'Flower Boy',
+    c: 'IGOR',
+    d: 'Cherry Bomb',
+    answer: 'c'
+  },
+  {
+    question: 'Who is better known as Lil Wayne?', 
+    a: 'Wayne Michael Carter Jr',
+    b: 'Michael Wayne Carter Jr',
+    c: 'Michael Dwayne Carter Jr',
+    d: 'Dwayne Michael Carter Jr',
+    answer: 'd'
+  },
+  {
+    question: 'Where was Kid Cudi born?', 
+    a: 'Houston TX',
+    b: 'Cleveland OH',
+    c: 'Brooklyn NY',
+    d: 'Atlanta GA',
+    answer: 'b'
+  },
+];
