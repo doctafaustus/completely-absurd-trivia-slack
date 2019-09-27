4fdbd8d186f5920bdae4803e64a86c4d1eb1fea4
doctafaustus <clowd150@gmail.com>
2019-09-27 00:45:25 -0500

    Add and configure qs 44

---
+++ questions/question-set-44.js
@@ -0,0 +1,82 @@
+module.exports = [
+  {
+    question: 'Bryant Gumbel is best known for hosting what show?', 
+    a: 'Nightline',
+    b: 'Let\'s Make A Deal',
+    c: 'NBC\'s "Today"',
+    d: 'The Weakest Link',
+    answer: 'c'
+  },
+  {
+    question: 'Motown records started in which U.S. city?', 
+    a: 'Detroit',
+    b: 'Atlanta',
+    c: 'New York',
+    d: 'Nashville',
+    answer: 'a'
+  },
+  {
+    question: 'What TV shows was a spinoff of Cheers and featured David Hyde Pierce?', 
+    a: 'Spin City',
+    b: 'Step by Step',
+    c: 'Mad About You',
+    d: 'Frasier',
+    answer: 'd'
+  },
+  {
+    question: 'What term describes the temperature at which cooled water vapor turns into liquid water?', 
+    a: 'Saturation Point',
+    b: 'Dew Point',
+    c: 'Evaporation',
+    d: 'Relative Humidity',
+    answer: 'b'
+  },
+  {
+    question: 'Who painted the 1930 painting "American Gothic"?', 
+    a: 'Grant Wood',
+    b: 'Edvard Munch',
+    c: 'Vincent van Gogh',
+    d: 'Salvador Dali',
+    answer: 'a'
+  },
+  {
+    question: 'What Nu Metal band formed in 1993 and features Jonathan Davis on vocals?', 
+    a: 'Limp Bizkit',
+    b: 'Korn',
+    c: 'Deftones',
+    d: 'Papa Roach',
+    answer: 'b'
+  },
+  {
+    question: 'How many calories are in a McDonald\'s Big Mac?', 
+    a: '390',
+    b: '540',
+    c: '780',
+    d: '860',
+    answer: 'b'
+  },
+  {
+    question: 'What organization assigns top-level domains like ".com", ".org", and ".net"?', 
+    a: 'IANA',
+    b: 'IETF',
+    c: 'ICANN',
+    d: 'IMAP',
+    answer: 'c'
+  },
+  {
+    question: 'The album "40 Oz. To Freedom" was produced in 1992 by which band?', 
+    a: 'Sublime',
+    b: 'No Doubt',
+    c: 'Nirvana',
+    d: 'Weezer',
+    answer: 'a'
+  },
+  {
+    question: 'Also known as "Zone Therapy," which alternative medicine involves applying pressure to the hands or feet?', 
+    a: 'Homeopathy',
+    b: 'Shiatsu',
+    c: 'Reiki',
+    d: 'Reflexology',
+    answer: 'd'
+  },
+];
