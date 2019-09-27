fa0d56973a5c778bdd48ef52f52335626f87aa9b
doctafaustus <clowd150@gmail.com>
2019-09-27 11:20:20 -0500

    Add qs 45

---
+++ questions/question-set-45.js
@@ -0,0 +1,82 @@
+module.exports = [
+  {
+    question: 'The Ayatollah Khomeini was the religious leader of which country during the 1980s?', 
+    a: 'Iran',
+    b: 'Argentina',
+    c: 'Iraq',
+    d: 'India',
+    answer: 'a'
+  },
+  {
+    question: 'Which of these is a real language?', 
+    a: 'Europeaans',
+    b: 'Hollandiaans',
+    c: 'Antarctikaans',
+    d: 'Afrikaans',
+    answer: 'd'
+  },
+  {
+    question: 'Which country is also known as Burma?', 
+    a: 'Angora',
+    b: 'Myanmar',
+    c: 'Taiwan',
+    d: 'Thailand',
+    answer: 'b'
+  },
+  {
+    question: 'Which planet is the largest in size?', 
+    a: 'Uranus',
+    b: 'Neptune',
+    c: 'Saturn',
+    d: 'Mars',
+    answer: 'c'
+  },
+  {
+    question: 'Which country did the Magna Carta originate in?', 
+    a: 'France',
+    b: 'England',
+    c: 'Italy',
+    d: 'Spain',
+    answer: 'b'
+  },
+  {
+    question: 'What is the highest number on the pH scale?', 
+    a: '7',
+    b: '14',
+    c: '10',
+    d: '12',
+    answer: 'b'
+  },
+  {
+    question: 'What was in 1893 the first country to allow women to vote?', 
+    a: 'Canada',
+    b: 'United States',
+    c: 'Australia',
+    d: 'New Zealand',
+    answer: 'd'
+  },
+  {
+    question: 'Who wrote the novel "The Adventures of Huckleberry Finn"?', 
+    a: 'Harper Lee',
+    b: 'Mark Twain',
+    c: 'William Faulkner',
+    d: 'Ernest Hemingway',
+    answer: 'b'
+  },
+  {
+    question: 'A file with a ".rar" extension is most similar to which other type of file?', 
+    a: 'zip',
+    b: 'jpg',
+    c: 'sys',
+    d: 'txt',
+    answer: 'a'
+  },
+  {
+    question: 'Phil Collins song "You\'ll Be in My Heart" was featured in which Disney movie?', 
+    a: 'Aladdin',
+    b: 'Pocahontas',
+    c: 'The Little Mermaid',
+    d: 'Tarzan',
+    answer: 'd'
+  },
+];
